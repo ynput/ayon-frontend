ae3dbe1e7a3ea020bee216c61fd6122956709f77
Martastain <martas@imm.cz>
2022-04-27 15:59:40 +0200

    styled anatomy editor

--- src/containers/anatomyEditor/customFields.js
+++ src/containers/anatomyEditor/customFields.js
@@ -1,12 +1,11 @@
-import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
-import { MultiSelect } from 'primereact/multiselect';
-
+import { InputText } from 'primereact/inputtext'
+import { InputNumber } from 'primereact/inputnumber'
+import { MultiSelect } from 'primereact/multiselect'
 
 const PrimeInputText = (props) => {
   return (
-    <InputText 
-      style={{width: '100%'}}
+    <InputText
+      style={{ width: '100%' }}
       id={props.id}
       readOnly={props.readOnly}
       value={props.formData}
@@ -17,14 +16,13 @@ const PrimeInputText = (props) => {
   )
 }
 
-
 const PrimeInputNumber = (props) => {
   const sets = {}
   if (props.schema.exclusiveMinimum !== undefined)
     sets.min = props.schema.exclusiveMinimum + 1
   return (
-    <InputNumber 
-      style={{width: '100%'}}
+    <InputNumber
+      style={{ width: '100%' }}
       id={props.id}
       readOnly={props.readOnly}
       value={props.formData}
@@ -32,11 +30,13 @@ const PrimeInputNumber = (props) => {
       onBlur={props.onBlur}
       onFocus={props.onFocus}
       showButtons={true}
+      className={props.rawErrors ? 'p-invalid' : ''}
+      tooltip={props.rawErrors ? props.rawErrors[0] : undefined}
+      tooltipOptions={{ position: 'bottom' }}
       {...sets}
     />
   )
 }
-
 
 const PrimeMultiSelect = (props) => {
   console.log(props)
@@ -50,40 +50,39 @@ const PrimeMultiSelect = (props) => {
     options = enmlist.map((value, index) => {
       return {
         label: value,
-        value: value
+        value: value,
       }
     })
   }
 
   return (
-      <div className="settings-field">
-        <div className="p-inputgroup">
-          <span className="p-inputgroup-addon settings-field-label">{props.schema.title}</span>
-          <MultiSelect 
-            style={{width: '100%'}}
-            id={props.id}
-            readOnly={props.readOnly}
-            value={props.formData}
-            options={options}
-            onChange={(event) => props.onChange(event.value)}
-            onBlur={props.onBlur}
-            onFocus={props.onFocus}
-            scrollHeight='400px'
-            maxSelectedLabels={3}
-          />
-        </div>  
-        <small className="p-error block">{props.rawErrors || <>&nbsp;</>}</small>
+    <div className="settings-field">
+      <div className="p-inputgroup">
+        <span className="p-inputgroup-addon settings-field-label">
+          {props.schema.title}
+        </span>
+        <MultiSelect
+          style={{ width: '100%' }}
+          id={props.id}
+          readOnly={props.readOnly}
+          value={props.formData}
+          options={options}
+          onChange={(event) => props.onChange(event.value)}
+          onBlur={props.onBlur}
+          onFocus={props.onFocus}
+          scrollHeight="400px"
+          maxSelectedLabels={3}
+        />
       </div>
+      <small className="p-error block">{props.rawErrors || <>&nbsp;</>}</small>
+    </div>
   )
-
 }
-
 
 const customFields = {
   StringField: PrimeInputText,
   NumberField: PrimeInputNumber,
   multiselect: PrimeMultiSelect,
 }
-
 
 export default customFields
--- src/containers/anatomyEditor/index.js
+++ src/containers/anatomyEditor/index.js
@@ -1,4 +1,4 @@
-import Form from "@rjsf/core"
+import Form from '@rjsf/core'
 
 import { Accordion, AccordionTab } from 'primereact/accordion'
 
@@ -8,94 +8,90 @@ import customFields from './customFields'
 
 import './anatomy.sass'
 
-
 const WrapperTemplate = (props) => {
   return (
-    <Accordion multiple transitionOptions={{ timeout: 0 }} >
-      {props.properties.map(element => {
-        return <AccordionTab 
-          key={element.name}
-          header={element.content.props.schema.title}
-        >
-          
-          <div style={{padding: '1em 0'}}>
-            {element.content}
-          </div>
-        </AccordionTab>
-        }
-      )}
+    <Accordion multiple transitionOptions={{ timeout: 0 }}>
+      {props.properties.map((element) => {
+        return (
+          <AccordionTab
+            key={element.name}
+            header={element.content.props.schema.title}
+          >
+            <>{element.content}</>
+          </AccordionTab>
+        )
+      })}
     </Accordion>
-  );
+  )
 }
 
 const TopLevelObjectFieldTemplate = (props) => {
-  return (
-    <div>
-      {props.properties.map(element => element.content)}
-    </div>
-  );
+  return <div>{props.properties.map((element) => element.content)}</div>
 }
-
 
 const SettingsFieldTemplate = (props) => {
   if (props.displayLabel)
     return (
       <div className="settings-field">
         <div className="p-inputgroup">
-          {props.displayLabel && <span className="p-inputgroup-addon settings-field-label">{props.label} </span>}
+          {props.displayLabel && (
+            <span className="p-inputgroup-addon settings-field-label">
+              {props.label}{' '}
+            </span>
+          )}
           {props.children}
-        </div>  
-        <small className="p-error block">{props.rawErrors || <>&nbsp;</>}</small>
+        </div>
       </div>
     )
 
   return props.children
 }
 
-
-const AnatomyEditor = ({schema, formData, onChange}) => {
+const AnatomyEditor = ({ schema, formData, onChange }) => {
   const topLevelFields = {
-      "ui:ObjectFieldTemplate": TopLevelObjectFieldTemplate,
+    'ui:ObjectFieldTemplate': TopLevelObjectFieldTemplate,
   }
 
   const uischema = {
-    "ui:rootFieldId": "jform",
-    "ui:ObjectFieldTemplate": WrapperTemplate,
-    "roots" : compactListUiSchema,
+    'ui:rootFieldId': 'jform',
+    'ui:ObjectFieldTemplate': WrapperTemplate,
+    roots: compactListUiSchema,
 
-    "templates": {
+    templates: {
       ...topLevelFields,
-      "work": templateGroupUiSchema,
-      "render": templateGroupUiSchema,
-      "publish": templateGroupUiSchema,
-      "hero": templateGroupUiSchema,
-      "delivery": templateGroupUiSchema,
-      "others": templateGroupUiSchema,
+      work: templateGroupUiSchema,
+      render: templateGroupUiSchema,
+      publish: templateGroupUiSchema,
+      hero: templateGroupUiSchema,
+      delivery: templateGroupUiSchema,
+      others: templateGroupUiSchema,
     },
 
-    "attributes": {
+    attributes: {
       ...topLevelFields,
       applications: {
-        "ui:field": "multiselect"
-      }
+        'ui:field': 'multiselect',
+      },
     },
 
-    "folder_types": compactListUiSchema,
-    "task_types": compactListUiSchema,
-}
+    folder_types: compactListUiSchema,
+    task_types: compactListUiSchema,
+  }
 
   return (
     <>
-    {schema && <Form 
-        schema={schema} 
-        formData={formData}
-        uiSchema={uischema}
-        onChange={(evt) => onChange(evt.formData)}
-        fields={customFields}
-        liveValidate={true}
-        FieldTemplate={SettingsFieldTemplate}
-        children={<></>}
-      />}
+      {schema && (
+        <Form
+          schema={schema}
+          formData={formData}
+          uiSchema={uischema}
+          onChange={(evt) => onChange(evt.formData)}
+          fields={customFields}
+          liveValidate={true}
+          FieldTemplate={SettingsFieldTemplate}
+          children={<></>}
+        />
+      )}
     </>
   )
 }
