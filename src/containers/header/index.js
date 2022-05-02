217c6b05a52c999a1d9a35b7671e89858f31c74b
Martastain <martas@imm.cz>
2022-05-02 13:49:56 +0200

    feat: sidebar project list

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,8 +1,10 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import { Button, Spacer } from '../../components'
-
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
 import {Sidebar} from 'primereact/sidebar'
+import axios from 'axios'
 
 import Breadcrumbs from './breadcrumbs'
 import UserMenu from './userMenu'
@@ -10,6 +12,19 @@ import UserMenu from './userMenu'
 
 const SidebarMenu = ({visible, onHide}) => {
   const navigate = useNavigate()
+  const [projectList, setProjectList] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('/api/projects')
+      .then((response) => {
+        setProjectList(response.data.projects || [])
+      })
+      .catch(() => {
+        console.error.error('Unable to load projects')
+      })
+  }, [])
+
   return(
     <Sidebar
         position="left"
@@ -22,9 +37,26 @@ const SidebarMenu = ({visible, onHide}) => {
         flexDirection: 'column',
       }}
     >
+      <div style={{ flexGrow: 1}}>
+          <DataTable
+            value={projectList}
+            scrollable
+            scrollHeight="flex"
+            selectionMode="single"
+            responsive="true"
+            dataKey="name"
+            onSelectionChange={(e) => {
+              navigate(`/projects/${e.value.name}/browser`)
+              onHide()
+            }}
+          >
+            <Column field="name" header="Name" />
+          </DataTable>
+    </div>
 
     <Button 
       label="Project manager"
+      style={{ marginTop: 40}}
       onClick={() => {
         navigate('/projects')
         onHide()
