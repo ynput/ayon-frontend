a248464e7d5e034e405da98d28daf820b4584e13
Martastain <martas@imm.cz>
2022-05-10 17:34:33 +0200

    feat: list-to-tree transform

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+
 const arrayEquals = (a, b) => {
   return (
     Array.isArray(a) &&
@@ -7,6 +8,75 @@ const arrayEquals = (a, b) => {
     a.length === b.length &&
     a.every((val, index) => val === b[index])
   )
+}
+
+
+const sortByKey = (array, key) => {
+  /* sort an array of objects by a given key */
+  return array.sort(function (a, b) {
+    var x = a[key]
+    var y = b[key]
+    return x < y ? -1 : x > y ? 1 : 0
+  })
+}
+
+
+const isGroupable = (data, key, value) => {
+  // Returns true if the key with the given value is 
+  // presented multiple times in the array
+  // and therefore can be grouped.
+  let count = 0
+  for (const item of data) {
+    if (item[key] === value){
+      count++
+      if (count > 1)
+        return true
+    }
+  }
+  return false
+}
+
+
+const groupResult = (data, groupBy, key="id") => {
+  // Transform a list of records to a TreeTable-compatible structure
+  // with grouped records.
+  let result = []
+  let existingGroups = []
+  for (const item of data) {
+    // Unique items. Just add to root
+    if (!isGroupable(data, groupBy, item[groupBy])) {
+      result.push({ key: item[key], data: item })
+    } 
+
+    // Item of an existing group
+    else if (existingGroups.includes(item[groupBy])) {
+      for (const group of result) {
+        if (group.data[groupBy] === item[groupBy]) {
+          group.children.push({
+            key: item[key],
+            data: item
+          })
+          break
+        }
+      }
+    } 
+
+    // New group
+    else {
+      existingGroups.push(item[groupBy])
+      result.push({
+        key: `group-${item[groupBy]}`,
+        data: {
+          [groupBy]: item[groupBy]
+        },
+        children: [{
+          key: item[key],
+          data: item
+        }]
+      })
+    }
+  }
+  return result
 }
 
 const loadAnatomyPresets = async () => {
@@ -32,4 +102,4 @@ const loadAnatomyPresets = async () => {
   return [primaryPreset, ...presets]
 }
 
-export { arrayEquals, loadAnatomyPresets }
+export { arrayEquals, loadAnatomyPresets, groupResult, sortByKey }
