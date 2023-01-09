13c75cb63c4aac93bd915e550bc32e2d376b8189
Innders <49156310+Innders@users.noreply.github.com>
2023-01-09 09:18:51 +0000

    debounce multiple ws

--- src/hooks/usePubSub.js
+++ src/hooks/usePubSub.js
@@ -1,13 +1,14 @@
+import { debounce } from 'lodash'
 import { useEffect } from 'react'
 import PubSub from '/src/pubsub'
 
 const usePubSub = (topic, callback, ids) => {
-  const handlePubSub = (topicName, message) => {
+  const handlePubSub = debounce((topicName, message) => {
     if (ids && !ids.includes(message?.summary?.entityId)) return
     console.log('WS Version Refetch', topicName)
 
     callback(topicName, message)
-  }
+  }, 100)
 
   useEffect(() => {
     const token = PubSub.subscribe(topic, handlePubSub)
