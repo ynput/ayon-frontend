09507c27cacaf61f3abfcb29e3d42c86bbafba3c
Innders <49156310+Innders@users.noreply.github.com>
2023-01-27 14:21:19 +0000

    chore: breakout event list

--- src/pages/event/EventPage.jsx
+++ src/pages/event/EventPage.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import { Section, Toolbar, TablePanel } from '@ynput/ayon-react-components'
-import { TimestampField } from '/src/containers/fieldFormat'
-import { DataTable } from 'primereact/datatable'
-import { Column } from 'primereact/column'
+import { Section } from '@ynput/ayon-react-components'
 import usePubSub from '/src/hooks/usePubSub'
 import { useGetEventsQuery } from '/src/services/events/getEvents'
 import EventDetailDialog from './EventDetail'
 import { useDispatch } from 'react-redux'
 import { ayonApi } from '/src/services/ayon'
+import { Splitter, SplitterPanel } from 'primereact/splitter'
+import EventList from './EventList'
 
 const EventPage = () => {
   const dispatch = useDispatch()
   const [selectedEvent, setSelectedEvent] = useState(null)
-  const [detailVisible, setDetailVisible] = useState(false)
 
   const last = 100
   const { data: eventData, isLoading, isError, error } = useGetEventsQuery({ last })
@@ -39,16 +37,6 @@ const EventPage = () => {
 
   usePubSub('*', handlePubSub)
 
-  const formatTime = (rowData) => {
-    return <TimestampField value={rowData.updatedAt} />
-  }
-
-  const onRowClick = (e) => {
-    if (e.originalEvent.detail === 2) {
-      setDetailVisible(true)
-    }
-  }
-
   // handle error
   if (isError) {
     return <div>Error: {error.message}</div>
@@ -56,38 +44,20 @@ const EventPage = () => {
 
   return (
     <main>
-      {detailVisible && (
-        <EventDetailDialog onHide={() => setDetailVisible(false)} id={selectedEvent?.id} />
-      )}
       <Section>
-        <Toolbar></Toolbar>
-        <TablePanel loading={isLoading}>
-          <DataTable
-            value={eventData}
-            scrollable="true"
-            scrollHeight="flex"
-            responsive="true"
-            dataKey="id"
-            selectionMode="single"
-            onSelectionChange={(e) => setSelectedEvent(e.value)}
-            selection={selectedEvent}
-            onRowClick={onRowClick}
-            rowClassName={(rowData) => {
-              return {
-                highlight: selectedEvent && selectedEvent.dependsOn === rowData.id,
-                'row-error': rowData.topic === 'log.error',
-                'row-warning': rowData.topic === 'log.warning',
-                'row-success': rowData.topic === 'log.success',
-              }
-            }}
-          >
-            <Column header="Time" body={formatTime} style={{ maxWidth: 180 }} />
-            <Column header="Topic" field="topic" style={{ maxWidth: 200 }} />
-            <Column header="Description" field="description" />
-            <Column header="User" field="user" style={{ maxWidth: 120 }} />
-            <Column header="Project" field="project" style={{ maxWidth: 150 }} />
-          </DataTable>
-        </TablePanel>
+        <Splitter style={{ height: '100%', width: '100%' }}>
+          <SplitterPanel size={70}>
+            <EventList
+              eventData={eventData}
+              isLoading={isLoading}
+              selectedEvent={selectedEvent}
+              setSelectedEvent={setSelectedEvent}
+            />
+          </SplitterPanel>
+          <SplitterPanel size={30}>
+            <EventDetailDialog id={selectedEvent?.id} />
+          </SplitterPanel>
+        </Splitter>
       </Section>
     </main>
   )
