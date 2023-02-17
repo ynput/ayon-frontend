fc181da3eeba6d83500d0a40ed68a9b8cc3eeb00
Innders <49156310+Innders@users.noreply.github.com>
2023-02-17 09:54:56 +0000

    chore: remove stats counting animation

--- src/pages/projectDashboard/panels/ProjectStats.jsx
+++ src/pages/projectDashboard/panels/ProjectStats.jsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import { useMemo } from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useMemo } from 'react'
 import DashboardPanelWrapper from './DashboardPanelWrapper'
 import ListStatsTile from './ListStatsTile'
 import copyToClipboard from '/src/helpers/copyToClipboard'
 import { useGetProjectDashboardQuery } from '/src/services/getProjectDashboard'
 
 const ProjectStats = ({ projectName, share, position }) => {
-  const [counters, setCounters] = useState({})
-  const [isCounting, setIsCounting] = useState(true)
-
   const {
     data = {},
     isLoading,
@@ -19,49 +13,6 @@ const ProjectStats = ({ projectName, share, position }) => {
 
   const { folders, subsets, tasks, versions, representations, workfiles } = data
 
-  useEffect(() => {
-    // when data loaded use a setInterval to count up to the actual number
-    const intervals = 100
-    let count = 0
-    let interval
-    if (!isLoading) {
-      let tempCounters = {
-        folders: 0,
-        subsets: 0,
-        tasks: 0,
-        versions: 0,
-        representations: 0,
-        workfiles: 0,
-      }
-      setIsCounting(true)
-
-      interval = setInterval(() => {
-        count++
-
-        tempCounters = {
-          folders: Math.round((folders / intervals) * count),
-          subsets: Math.round((subsets / intervals) * count),
-          tasks: Math.round((tasks / intervals) * count),
-          versions: Math.round((versions / intervals) * count),
-          representations: Math.round((representations / intervals) * count),
-          workfiles: Math.round((workfiles / intervals) * count),
-        }
-
-        setCounters(tempCounters)
-        if (count === intervals) {
-          setIsCounting(false)
-          clearInterval(interval)
-        }
-      }, 5)
-    }
-
-    //   clear
-    return () => {
-      clearInterval(interval)
-    }
-  }, [isLoading, data])
-
-  // convert above to object
   const stats = {
     folders: { label: 'Folders', icon: 'folder', stat: folders },
     subsets: { label: 'Subsets', icon: 'inventory_2', stat: subsets },
@@ -82,7 +33,7 @@ const ProjectStats = ({ projectName, share, position }) => {
 
   const shareData = useMemo(() => {
     return { project: projectName, ...data }
-  }, [isCounting])
+  }, [data])
 
   return (
     <DashboardPanelWrapper
@@ -96,7 +47,7 @@ const ProjectStats = ({ projectName, share, position }) => {
         return (
           <ListStatsTile
             title={label}
-            stat={counters[id] || stats[id].stat}
+            stat={stats[id].stat}
             icon={icon}
             isLoading={isLoading}
             key={id}
