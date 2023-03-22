9f18d2198616d4c9d8a2f5988b3a395b45f9b711
Innders <49156310+Innders@users.noreply.github.com>
2023-03-22 11:38:24 +0000

    fix: date formatting

--- src/containers/attributeTable.jsx
+++ src/containers/attributeTable.jsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 import styled from 'styled-components'
 import TableRow from '../components/TableRow'
 import { useGetAttributesQuery } from '../services/getAttributes'
@@ -26,9 +27,15 @@ const AttributeTable = ({ entityType, data, additionalData, style, projectAttrib
         ))}
 
       {data &&
-        attribFields.map(({ name, data: attribData = {} }) => (
-          <TableRow key={name} value={data[name]} name={attribData.title} />
-        ))}
+        attribFields.map(({ name, data: attribData = {} }) => {
+          let value = data[name]
+
+          if (value && name.includes('Date')) {
+            value = format(new Date(value), 'dd/MM/yyyy')
+          }
+
+          return <TableRow key={name} value={value} name={attribData.title} />
+        })}
 
       {projectAttrib &&
         projectAttrib.map(({ name, value }) => <TableRow key={name} name={name} value={value} />)}
