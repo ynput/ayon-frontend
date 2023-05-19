2f285bae691c7202ab31166995775338645cbc3a
Martin Wacker <martas@imm.cz>
2023-05-19 15:45:09 +0200

    focus entities in project page based on pasted uri (WIP0

--- src/containers/header/breadcrumbs.jsx
+++ src/containers/header/breadcrumbs.jsx
@@ -1,4 +1,16 @@
-import { useSelector } from 'react-redux'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { InputText, Button } from '@ynput/ayon-react-components'
+
+import {
+  setFocusedFolders,
+  setFocusedSubsets,
+  setFocusedVersions,
+  setUri,
+} from '/src/features/context'
 
 import styled from 'styled-components'
 
@@ -32,127 +44,83 @@ const Crumbtainer = styled.div`
     }
   }
 `
-/*
-const ProjectBreadcrumbs = ({ crumbData, projectName }) => {
-  const [breadcrumbs, uri] = useMemo(() => {
-    let crumbs = [projectName]
-    let uri = `ayon://${projectName}/`
-    for (const h of crumbData.parents || []) {
-      crumbs.push(h)
-      uri += `${h}/`
-    }
-    if (crumbData.folder) {
-      crumbs.push(crumbData.folder)
-      uri += crumbData.folder
-    }
 
-    if (crumbData.subset) {
-      crumbs.push(crumbData.subset)
-      uri += `?subset=${crumbData.subset}`
-    }
-
-    if (crumbData.version) {
-      crumbs.push(crumbData.version)
-      uri += `?version=${crumbData.version}`
-    }
-
-    if (crumbData.representation) {
-      crumbs.push(crumbData.representation)
-      uri += `?representation=${crumbData.representation}`
-    }
-
-    return [crumbs, uri]
-  }, [
-    crumbData.parents,
-    crumbData.folder,
-    crumbData.subset,
-    crumbData.version,
-    crumbData.representation,
-    projectName,
-  ])
-
-  const copyURI = () => {
-    navigator.clipboard.writeText(uri).then(
-      () => {
-        toast.success('URI copied')
-      },
-      (err) => {
-        toast.error('Could not copy text: ', err)
-      },
-    )
-  }
-
-  if (!projectName) return <></>
-
-  return (
-    <Crumbtainer onClick={copyURI}>
-      <ul>
-        {breadcrumbs.map((crumb, index) => (
-          <li key={index}>{crumb}</li>
-        ))}
-      </ul>
-    </Crumbtainer>
-  )
-}
-
-const SettingsBreadcrumbs = ({ crumbData }) => {
-  let crumbs = []
-  if (crumbData?.addonName && crumbData?.addonVersion) {
-    crumbs.push(`${crumbData.addonName}@${crumbData.addonVersion}`)
-  } else if (crumbData?.addonName) {
-    crumbs.push(crumbData.addonName)
-  }
-
-  crumbs = [...crumbs, ...(crumbData.path || [])]
-
-  const copyBreadcrumbs = () => {
-    navigator.clipboard.writeText(crumbs.join('/')).then(
-      () => toast.success('Breadcrumbs copied'),
-      (err) => toast.error('Could not copy text: ', err),
-    )
-  }
-
-  return (
-    <Crumbtainer onClick={copyBreadcrumbs}>
-      <ul>
-        {crumbs.map((crumb, index) => (
-          <li key={index}>{crumb}</li>
-        ))}
-      </ul>
-    </Crumbtainer>
-  )
-}
-*/
 const Breadcrumbs = () => {
-  /*
-    Breadcrums component used in the browser view.
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    Current location is taken from the redux store as an Object
-    containing all the components of the current path.
-    This allows to render the breadcrumbs as well as compile the
-    op:// USD uri.
-    */
+  const [localUri, setLocalUri] = useState('')
+  const ctxUri = useSelector((state) => state.context.uri) || ''
 
-  const uri = useSelector((state) => state.context.uri) || ''
+  const focusEntities = (entities) => {
+    const focusedFolders = []
+    const focusedSubsets = []
+    const focusedVersions = []
 
-  return <Crumbtainer>{uri}</Crumbtainer>
+    const project = entities[0].projectName
 
-  /*
+    // assert we current url starts with projects/<projectName>
+    // if not, redirect
 
-  const crumbData = useSelector((state) => state.context.breadcrumbs)
-  const projectName = useSelector((state) => state.project.name)
+    const path = window.location.pathname
+    if (!path.startsWith(`/projects/${project}`)) {
+      navigate(`/projects/${project}/browser`)
+    }
 
-  const scope = crumbData.scope
+    for (const entity of entities) {
+      if (entity.folderId) focusedFolders.push(entity.folderId)
+      if (entity.subsetId) focusedSubsets.push(entity.subsetId)
+      if (entity.versionId) focusedVersions.push(entity.versionId)
 
-  if (scope === 'project') {
-    return <ProjectBreadcrumbs crumbData={crumbData} projectName={projectName} />
-  } else if (scope === 'settings') {
-    return <SettingsBreadcrumbs crumbData={crumbData} />
+      if (entity.projectName !== project) {
+        toast.error('Entities must be from the same project')
+        continue
+      }
+    }
+
+    dispatch(setFocusedFolders(focusedFolders))
+    dispatch(setFocusedSubsets(focusedSubsets))
+    dispatch(setFocusedVersions(focusedVersions))
   }
 
-  return null
-  // return JSON.stringify(crumbData)
- */
+  const goThere = () => {
+    axios
+      .post('/api/resolve', { uris: [localUri] })
+      .then((res) => {
+        if (!res.data.length) {
+          toast.error('Could not resolve uri')
+          return
+        }
+        const entities = res.data[0].entities
+        if (!entities.length) {
+          toast.error('No entities found')
+          return
+        }
+        focusEntities(entities)
+        setTimeout(() => {
+          dispatch(setUri(res.data[0].uri))
+        }, 100)
+      })
+      .catch((err) => {
+        toast.error(err)
+      })
+  }
+
+  useEffect(() => {
+    if (ctxUri === localUri) return
+    setLocalUri(ctxUri)
+  }, [ctxUri])
+
+  return (
+    <Crumbtainer>
+      <InputText
+        value={localUri}
+        onChange={(e) => setLocalUri(e.target.value)}
+        style={{ width: 800 }}
+      />
+      <Button onClick={() => goThere()}>Go</Button>
+    </Crumbtainer>
+  )
 }
 
 export default Breadcrumbs
