95ae5ae29748068cece7b7f330305d1f82b08413
Martin Wacker <martas@imm.cz>
2023-06-08 17:51:37 +0200

    icon for event status, added event restart button

--- src/pages/EventsPage/EventList.jsx
+++ src/pages/EventsPage/EventList.jsx
@@ -3,6 +3,34 @@ import { Section, TablePanel } from '@ynput/ayon-react-components'
 import { TimestampField } from '/src/containers/fieldFormat'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
+import { CellWithIcon } from '/src/components/icons'
+
+import styled, { keyframes } from 'styled-components'
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const InlineSpinner = styled.div`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  border: 2px solid rgba(255, 255, 255, 0.3);
+  border-top-color: #fff;
+  animation: ${spin} 0.8s linear infinite;
+`
+
+const statusBodyTemplate = (rowData) => {
+  if (rowData.status === 'finished') return <CellWithIcon icon="check" />
+  if (rowData.status === 'aborted') return <CellWithIcon icon="times" />
+  if (rowData.status === 'failed') return <CellWithIcon icon="error" />
+  if (rowData.status === 'in_progress') return <InlineSpinner />
+  if (rowData.status === 'pending') return <CellWithIcon icon="timer" />
+  if (rowData.status === 'restarted') return <CellWithIcon icon="history" />
+}
 
 const EventList = ({ eventData, isLoading, selectedEvent, setSelectedEvent, onScrollBottom }) => {
   const dataTableRef = useRef(null)
@@ -69,9 +97,10 @@ const EventList = ({ eventData, isLoading, selectedEvent, setSelectedEvent, onSc
           }}
           ref={dataTableRef}
         >
+          <Column style={{ maxWidth: 30 }} body={statusBodyTemplate} />
           <Column
             header="Time"
-            body={(row) => <TimestampField value={row.updatedAt} />}
+            body={(row) => <TimestampField value={row?.updatedAt} />}
             style={{ maxWidth: 180 }}
           />
           <Column header="Topic" field="topic" style={{ maxWidth: 200 }} />
