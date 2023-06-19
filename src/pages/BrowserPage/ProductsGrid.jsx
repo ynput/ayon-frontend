7228157bd322ee902723b9d93a613cf83e8093fd
Innders <49156310+Innders@users.noreply.github.com>
2023-06-19 07:59:07 +0100

    cosmetic: no products found

--- src/pages/BrowserPage/ProductsGrid.jsx
+++ src/pages/BrowserPage/ProductsGrid.jsx
@@ -16,9 +16,53 @@ const StyledGridLayout = styled.div`
   }
 `
 
+const NoneFound = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+// stack grid tiles on top of each other
+const StackedGridTiles = styled.div`
+  height: 120px;
+  margin-bottom: 32px;
+  & > * {
+    position: absolute;
+    height: 120px;
+    opacity: 1;
+  }
+
+  /* rotate out like a fan */
+  & > *:nth-child(1) {
+    transform: rotate(-10deg) translateX(-10px);
+    transform-origin: bottom;
+  }
+
+  & > *:nth-child(2) {
+    transform: rotate(10deg) translateX(10px);
+    transform-origin: bottom;
+  }
+`
+
 //  eslint-disable-next-line no-unused-vars
 const ProductsGrid = ({ isLoading, data, onSelection, onContext, selected, productTypes }) => {
   const statusesObject = useSelector((state) => state.project.statuses)
+  const isNone = data.length === 0 && !isLoading
+
+  if (isNone) {
+    return (
+      <StyledGridLayout>
+        <NoneFound>
+          <StackedGridTiles>
+            <EntityGridTile isError />
+            <EntityGridTile isError />
+          </StackedGridTiles>
+          <span>No products found</span>
+        </NoneFound>
+      </StyledGridLayout>
+    )
+  }
 
   return (
     <StyledGridLayout
@@ -28,7 +72,7 @@ const ProductsGrid = ({ isLoading, data, onSelection, onContext, selected, produ
     >
       <GridLayout ratio={1.5} minWidth={170}>
         {isLoading
-          ? Array.from({ length: 50 }).map((_, index) => (
+          ? Array.from({ length: 20 }).map((_, index) => (
               <EntityGridTile
                 style={{
                   minHeight: 'unset',
@@ -62,10 +106,12 @@ const ProductsGrid = ({ isLoading, data, onSelection, onContext, selected, produ
 }
 
 ProductsGrid.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-  data: PropTypes.array.isRequired,
-  onSelection: PropTypes.func.isRequired,
-  onContext: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool,
+  data: PropTypes.array,
+  onSelection: PropTypes.func,
+  onContext: PropTypes.func,
+  selected: PropTypes.string,
+  productTypes: PropTypes.object,
 }
 
 export default ProductsGrid
