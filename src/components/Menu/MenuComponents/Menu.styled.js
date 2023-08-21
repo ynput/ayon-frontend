38f2e61f7ff1df69f2ce28dd63811e56d594871b
Innders <49156310+Innders@users.noreply.github.com>
2023-08-21 15:26:31 +0100

    fix: menu scrolling

--- src/components/Menu/MenuComponents/Menu.styled.js
+++ src/components/Menu/MenuComponents/Menu.styled.js
@@ -14,6 +14,15 @@ export const Dialog = styled.dialog`
   padding: 0;
 
   user-select: none;
+
+  overflow: auto;
+  /* remove scroll bar */
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  padding-bottom: 60px;
 `
 
 const DialogOpenAnimation = keyframes`
@@ -63,10 +72,20 @@ export const Section = styled.section`
 `
 export const MenuWrapper = styled.div`
   position: relative;
+  border-radius: 8px;
 
   /* sub menu */
   &.sub-menu {
     position: absolute;
+    bottom: 0;
+    overflow: auto;
+    padding-bottom: 60px;
+    /* remove scroll bar */
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+    &::-webkit-scrollbar {
+      display: none;
+    }
     /* margin-right: 16px; */
 
     border-radius: 8px;
