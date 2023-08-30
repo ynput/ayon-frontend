658eb6b2a09d83dab8004ca5dfbc8e307eee41a0
Martin Wacker <martas@imm.cz>
2023-08-30 17:11:27 +0200

    settings copy: proof of concept

--- src/containers/AddonSettings/AddonSettings.jsx
+++ src/containers/AddonSettings/AddonSettings.jsx
@@ -27,105 +27,14 @@ import {
 } from '/src/services/addonSettings'
 
 import { usePromoteBundleMutation } from '/src/services/bundles'
-import { isEqual } from 'lodash'
 //import { useNavigate } from 'react-router'
 import { confirmDialog } from 'primereact/confirmdialog'
 
+import { getValueByPath, setValueByPath, sameKeysStructure, compareObjects } from './utils'
 /*
  * key is {addonName}|{addonVersion}|{environment}|{siteId}|{projectKey}
  * if project name or siteid is N/a, use _ instead
  */
-
-const getValueByPath = (obj, path) => {
-  // path is an array of keys
-  // e.g. ['a', 'b', 'c'] => obj.a.b.c
-  // if any key is not found, return undefined
-
-  if (path?.length === 0) return obj
-  let value = obj
-  for (const key of path) {
-    if (value === undefined) return undefined
-    value = value[key]
-  }
-  return value
-}
-
-const setValueByPath = (obj, path, value) => {
-  const result = { ...obj }
-  if (path?.length === 0) return value
-  let target = result
-  for (const key of path.slice(0, -1)) {
-    if (target[key] === undefined) target[key] = {}
-    target = target[key]
-  }
-  target[path[path.length - 1]] = value
-  return result
-}
-
-const sameKeysStructure = (obj1, obj2) => {
-  for (const type of ['string', 'number', 'boolean']) {
-    if (typeof obj1 === type && typeof obj2 === type) {
-      return true
-    }
-  }
-  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') return false
-  const obj1Keys = Object.keys(obj1)
-  const obj2Keys = Object.keys(obj2)
-  if (obj1Keys.length !== obj2Keys.length) {
-    console.warn('Len cond failed on ', obj1Keys, obj2Keys)
-    return false
-  }
-  for (const key of obj1Keys) {
-    // Let's allow this and see what happens
-    // if (!obj2Keys.includes(key)) return false
-
-    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-      if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) continue // just assume someone won't paste invalid array items here
-
-      if (!sameKeysStructure(obj1[key], obj2[key])) {
-        console.warn('Struct cond failed on ', obj1[key], obj2[key])
-        return false
-      }
-    }
-  }
-  return true
-}
-
-const compareObjects = (obj1, obj2, path = []) => {
-  // Compare two objects and return a list of 'paths' where the objects differ
-  const changedPaths = []
-  for (const key in obj1) {
-    const newPath = [...path, key]
-
-    if (!(key in obj2)) {
-      changedPaths.push(newPath)
-      continue
-    }
-
-    const value1 = obj1[key]
-    const value2 = obj2[key]
-
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      if (Array.isArray(value1) && Array.isArray(value2)) {
-        if (!isEqual(value1, value2)) {
-          changedPaths.push(newPath)
-        }
-      } else {
-        const nestedPaths = compareObjects(value1, value2, newPath)
-        changedPaths.push(...nestedPaths)
-      }
-    } else if (value1 !== value2) {
-      changedPaths.push(newPath)
-    }
-  }
-  for (const key in obj2) {
-    const newPath = [...path, key]
-    if (!(key in obj1)) {
-      changedPaths.push(newPath)
-    }
-  }
-  return changedPaths
-}
 
 const AddonSettings = ({ projectName, showSites = false }) => {
   //const navigate = useNavigate()
@@ -172,7 +81,8 @@ const AddonSettings = ({ projectName, showSites = false }) => {
   }, [uriChanged])
 
   const onSettingsLoad = (addonName, addonVersion, variant, siteId, data) => {
-    const key = `${addonName}|${addonVersion}|${variant}${siteId}|${projectKey}`
+    const key = `${addonName}|${addonVersion}|${variant}|${siteId}|${projectKey}`
+    if (key in localData) return
     setOriginalData((localData) => {
       localData[key] = data
       return { ...localData }
@@ -453,7 +363,19 @@ const AddonSettings = ({ projectName, showSites = false }) => {
             {bundleName}
           </span>
           <Spacer />
-          <CopySettingsButton bundleName={bundleName} variant={environment} />
+          <CopySettingsButton
+            bundleName={bundleName}
+            variant={environment}
+            disabled={canCommit}
+            localData={localData}
+            localOverrides={localOverrides}
+            setLocalData={setLocalData}
+            setLocalOverrides={setLocalOverrides}
+            setSelectedAddons={setSelectedAddons}
+            originalData={originalData}
+            setOriginalData={setOriginalData}
+            projectName={projectName}
+          />
           <Button
             icon="local_shipping"
             tooltip="rocket_launch"
@@ -505,6 +427,8 @@ const AddonSettings = ({ projectName, showSites = false }) => {
     setSelectedAddons(newSelection)
     setCurrentSelection(null)
   }
+
+  console.log('LocalData', localData)
 
   return (
     <Splitter layout="horizontal" style={{ width: '100%', height: '100%' }}>
--- src/containers/AddonSettings/CopySettings.jsx
+++ src/containers/AddonSettings/CopySettings.jsx
@@ -1,14 +1,21 @@
 import { useState, useEffect, useMemo } from 'react'
-import { Button, Dropdown, Toolbar, TablePanel } from '@ynput/ayon-react-components'
+import { toast } from 'react-toastify'
+import { Button, Dropdown, Toolbar, TablePanel, Spacer } from '@ynput/ayon-react-components'
 
 import { Dialog } from 'primereact/dialog'
 import { TreeTable } from 'primereact/treetable'
 import { Column } from 'primereact/column'
 
 import { useGetBundleListQuery } from '/src/services/bundles'
-import { useLazyGetAddonSettingsOverridesQuery } from '/src/services/addonSettings'
+import {
+  useLazyGetAddonSettingsQuery,
+  useLazyGetAddonSettingsOverridesQuery,
+} from '/src/services/addonSettings'
 
 import VariantSelector from './VariantSelector'
+import { setValueByPath } from './utils'
+
+import { cloneDeep } from 'lodash'
 
 const BundleDropdown = ({ bundleName, setBundleName }) => {
   const { data, isLoading, isError } = useGetBundleListQuery({})
@@ -29,12 +36,11 @@ const BundleDropdown = ({ bundleName, setBundleName }) => {
   )
 }
 
-const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle }) => {
+const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle, nodes, setNodes }) => {
   //
   // targetBundle is used just to get the list of addons
   //
 
-  const [nodes, setNodes] = useState([])
   const [selectedNodes, setSelectedNodes] = useState([])
 
   const {
@@ -45,7 +51,7 @@ const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle }) => {
 
   const [triggerGetOverrides] = useLazyGetAddonSettingsOverridesQuery()
 
-  useEffect(() => {
+  const loadNodes = async () => {
     if (bundlesLoading || bundlesError) return
 
     let sourceBundleData = {}
@@ -70,55 +76,55 @@ const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle }) => {
       if (addonName && Object.keys(sourceBundleData.addons).includes(addonName)) {
         const sourceAddonVersion = sourceBundleData.addons[addonName]
 
+        const children = []
+        const res = await triggerGetOverrides({
+          addonName,
+          addonVersion: sourceAddonVersion,
+          variant: sourceVariant,
+        })
+        for (const id in res.data) {
+          const override = res.data[id]
+          if (override.inGroup || override.type === 'branch') continue
+          const item = {
+            key: id,
+            data: {
+              path: override.path,
+              newValue: override.value,
+            },
+          }
+          children.push(item)
+        }
+
+        if (!children.length) continue
+
         addonList.push({
           key: `${addonName}-${sourceAddonVersion}`,
-          leaf: false,
           data: {
             addonName: addonName,
             addonVersion: sourceAddonVersion,
+            targetAddonVersion: targetBundleData.addons[addonName],
           },
+          children: children,
         })
       }
     }
 
     setNodes(addonList)
+  }
+
+  useEffect(() => {
+    loadNodes()
   }, [bundlesData, sourceBundle, sourceVariant, targetBundle])
 
-  const onExpand = async (event) => {
-    if (!event.node.children) {
-      let lazyNode = event.node
-      lazyNode.children = []
+  const formatVersionColumn = (rowData) => {
+    if (rowData.data.addonVersion === rowData.data.targetAddonVersion) {
+      return rowData.data.addonVersion
+    }
+    return `${rowData.data.addonVersion} ⟶ ${rowData.data.targetAddonVersion}`
+  }
 
-      const res = await triggerGetOverrides({
-        addonName: event.node.data.addonName,
-        addonVersion: event.node.data.addonVersion,
-        variant: sourceVariant,
-      })
-
-      console.log('res', res)
-      for (const id in res.data) {
-        const override = res.data[id]
-        if (override.inGroup || override.type === 'branch') continue
-        const item = {
-          key: id,
-          data: {
-            path: override.path.join(' / '),
-            newValue: override.value,
-          },
-        }
-        lazyNode.children.push(item)
-      }
-
-      let _nodes = nodes.map((node) => {
-        if (node.key === event.node.key) {
-          node = lazyNode
-        }
-
-        return node
-      })
-
-      setNodes(_nodes)
-    } // if no children
+  const formatPathColumn = (rowData) => {
+    return rowData.data?.path?.join(' / ')
   }
 
   return (
@@ -128,10 +134,11 @@ const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle }) => {
         selectionKeys={selectedNodes}
         onSelectionChange={(e) => setSelectedNodes(e.value)}
         value={nodes}
-        onExpand={onExpand}
+        emptyMessage="No settings to copy"
       >
         <Column field="addonName" header="Addon name" expander />
-        <Column field="path" header="Path" />
+        <Column field="version" header="Addon version" body={formatVersionColumn} />
+        <Column field="path" header="Path" body={formatPathColumn} />
         <Column field="currentValue" header="Current value" />
         <Column field="newValue" header="New value" />
       </TreeTable>
@@ -139,10 +146,96 @@ const CopySettingsTable = ({ sourceBundle, sourceVariant, targetBundle }) => {
   )
 }
 
-const CopySettingsButton = ({ bundleName, variant }) => {
+const CopySettingsButton = ({
+  bundleName,
+  variant,
+  disabled,
+  localData,
+  setLocalData,
+  localOverrides,
+  setLocalOverrides,
+  setSelectedAddons,
+  originalData,
+  setOriginalData,
+  projectName,
+}) => {
   const [dialogVisible, setDialogVisible] = useState(false)
   const [sourceBundle, setSourceBundle] = useState(bundleName)
   const [sourceVariant, setSourceVariant] = useState('production')
+  const [nodes, setNodes] = useState([])
+
+  const [triggerGetSettings] = useLazyGetAddonSettingsQuery()
+
+  const onDoTheMagic = async () => {
+    const newLocalData = cloneDeep(localData)
+    const newLocalOverrides = { ...localOverrides }
+    const newOriginalData = { ...originalData }
+    const newSelectedAddons = []
+
+    for (const node of nodes) {
+      console.log('Migrating node', node.data.addonName, node.data.addonVersion)
+
+      // Define the target addon
+      const siteId = '_'
+      const projectKey = projectName || '_'
+      const key = `${node.data.addonName}|${node.data.targetAddonVersion}|${variant}|${siteId}|${projectKey}`
+      const addon = {
+        name: node.data.addonName,
+        version: node.data.targetAddonVersion,
+        variant: variant,
+        siteId,
+        key,
+      }
+
+      // Get current settings of the target addon
+
+      let addonSettings = newLocalData[key]
+      let addonOverrides = []
+      if (!localData[key]) {
+        const res = await triggerGetSettings({
+          addonName: node.data.addonName,
+          addonVersion: node.data.targetAddonVersion,
+          projectName,
+          siteId,
+          variant,
+        })
+        newOriginalData[key] = res.data
+        addonSettings = cloneDeep(res.data)
+      }
+
+      // Iterate over the changes and apply them to the target addon
+
+      for (const change of node.children) {
+        if (change.data.newValue) {
+          addonSettings = setValueByPath(addonSettings, change.data.path, change.data.newValue)
+          console.log('Copied', change.data.path.join('/'), change.data.newValue)
+          addonOverrides.push(change.data.path)
+        } else console.warn('Not implemented copy', change.data.path.join('/'))
+      } // for change of node.children
+
+      newLocalData[key] = addonSettings
+      newLocalOverrides[key] = addonOverrides
+      newSelectedAddons.push(addon)
+    } // for node of nodes
+
+    console.log('NEw local data', newLocalData)
+    setOriginalData(newOriginalData)
+    setLocalData(newLocalData)
+    setLocalOverrides(newLocalOverrides)
+    setSelectedAddons(newSelectedAddons)
+    toast.success('Settings copied')
+  }
+
+  //
+  // Render
+  //
+
+  const footer = (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <Spacer />
+      <Button label="Copy settings" icon="input" onClick={() => onDoTheMagic()} />
+    </div>
+  )
 
   return (
     <>
@@ -150,14 +243,15 @@ const CopySettingsButton = ({ bundleName, variant }) => {
         icon="input"
         tooltip="Copy bundle settings from..."
         onClick={() => setDialogVisible(true)}
+        disabled={disabled || !bundleName}
       />
       {dialogVisible && (
         <Dialog
           header={`Copy ${bundleName} ${variant} settings from...`}
           visible
           onHide={() => setDialogVisible(false)}
-          style={{ width: '50vw' }}
-          footer={<Button label="Copy selected" onClick={() => setDialogVisible(false)} />}
+          style={{ width: '80%', height: '80%' }}
+          footer={footer}
         >
           <Toolbar>
             Source bundle
@@ -168,6 +262,8 @@ const CopySettingsButton = ({ bundleName, variant }) => {
             sourceBundle={sourceBundle}
             sourceVariant={sourceVariant}
             targetBundle={bundleName}
+            nodes={nodes}
+            setNodes={setNodes}
           />
         </Dialog>
       )}
