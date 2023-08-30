8a64a1dc5e99033e745188f0a54ca6adb6b3341a
Innders <49156310+Innders@users.noreply.github.com>
2023-08-30 15:45:02 +0100

    fix: auto create code and prevent create without

--- src/pages/ProjectManagerPage/NewProjectDialog/NewProjectDialog.jsx
+++ src/pages/ProjectManagerPage/NewProjectDialog/NewProjectDialog.jsx
@@ -2,7 +2,7 @@ import { useState, useMemo, useEffect } from 'react'
 import { Dialog } from 'primereact/dialog'
 import { toast } from 'react-toastify'
 
-import { Button, Spacer, InputText, Toolbar } from '@ynput/ayon-react-components'
+import { Spacer, InputText, Toolbar, SaveButton } from '@ynput/ayon-react-components'
 import SettingsEditor from '/src/containers/SettingsEditor'
 import AnatomyPresetDropdown from './AnatomyPresetDropdown'
 import {
@@ -14,6 +14,7 @@ import { useCreateProjectMutation } from '/src/services/project/updateProject'
 const NewProjectDialog = ({ onHide }) => {
   const [name, setName] = useState('')
   const [code, setCode] = useState('')
+  const [codeSet, setCodeSet] = useState(false)
   const [newAnatomy, setNewAnatomy] = useState(null)
   const [selectedPreset, setSelectedPreset] = useState(null)
 
@@ -31,7 +32,7 @@ const NewProjectDialog = ({ onHide }) => {
   // Logic
   //
 
-  const [createProject] = useCreateProjectMutation()
+  const [createProject, { isLoading }] = useCreateProjectMutation()
 
   const handleSubmit = () => {
     createProject({
@@ -50,6 +51,49 @@ const NewProjectDialog = ({ onHide }) => {
       })
   }
 
+  const createCode = (name) => {
+    if (name.length <= 4) return name.toLowerCase()
+    let code = name.toLowerCase()
+    if (name.includes('_')) {
+      const subwords = name.split('_')
+      code = subwords
+        .map((subword) => subword.charAt(0))
+        .join('')
+        .slice(0, 4)
+    } else {
+      const vowels = ['a', 'e', 'i', 'o', 'u']
+      const filteredWord = name
+        .split('')
+        .filter((char) => !vowels.includes(char))
+        .join('')
+      code = filteredWord.slice(0, 4)
+    }
+
+    // if there is a number at the end of the name, add it to the code
+    const lastChar = name.charAt(name.length - 1)
+    if (!isNaN(lastChar)) {
+      code += lastChar
+    }
+
+    return code
+  }
+
+  const handleNameChange = (e) => {
+    // replace spaces with underscores
+    const name = e.target.value.replace(/\s/g, '_')
+    setName(name)
+    if (!codeSet || code === '') {
+      const code = createCode(name)
+
+      setCode(code)
+    }
+  }
+
+  const handleCodeChange = (e) => {
+    setCode(e.target.value)
+    setCodeSet(true)
+  }
+
   //
   // Render
   //
@@ -66,7 +110,12 @@ const NewProjectDialog = ({ onHide }) => {
   const footer = (
     <Toolbar style={{}}>
       <Spacer />
-      <Button icon="add" label="Create Project" onClick={handleSubmit} />
+      <SaveButton
+        label="Create Project"
+        onClick={handleSubmit}
+        active={name && code}
+        saving={isLoading}
+      />
     </Toolbar>
   )
 
@@ -95,13 +144,10 @@ const NewProjectDialog = ({ onHide }) => {
             placeholder="Project Name"
             style={{ flexGrow: 1 }}
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={handleNameChange}
+            autoFocus
           />
-          <InputText
-            placeholder="Project code"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-          />
+          <InputText placeholder="Project code" value={code} onChange={handleCodeChange} />
           <AnatomyPresetDropdown
             selectedPreset={selectedPreset}
             setSelectedPreset={setSelectedPreset}
