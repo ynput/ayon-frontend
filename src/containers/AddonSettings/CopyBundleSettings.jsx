b692c3fc6bd13cbe842973c382bf59f434e70925
Martin Wacker <martas@imm.cz>
2023-09-14 16:02:50 +0200

    Show current production/staging state in bundledropdown

--- src/containers/AddonSettings/CopyBundleSettings.jsx
+++ src/containers/AddonSettings/CopyBundleSettings.jsx
@@ -56,12 +56,54 @@ const ToolSectionSeparator = styled.div`
   }
 `
 
+const BundleDropdownItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 8px;
+`
+
+const DropdownBadge = styled.span`
+  border-radius: 3px;
+  padding: 2px 4px;
+  font-size: 0.7rem;
+  font-weight: 600;
+  color: black;
+`
+
+const formatBundleDropdownItem = (bundle) => {
+  let badge = null
+  if (bundle.isProduction) {
+    badge = (
+      <DropdownBadge style={{ backgroundColor: 'var(--color-hl-production)' }}>
+        Production
+      </DropdownBadge>
+    )
+  } else if (bundle.isStaging) {
+    badge = (
+      <DropdownBadge style={{ backgroundColor: 'var(--color-hl-staging)' }}>Staging</DropdownBadge>
+    )
+  }
+
+  return (
+    <BundleDropdownItem>
+      {bundle.value} {badge}
+    </BundleDropdownItem>
+  )
+}
+
 const BundleDropdown = ({ bundleName, setBundleName, disabled }) => {
   const { data, isLoading, isError } = useGetBundleListQuery({})
 
   const bundleOptions = useMemo(() => {
     if (isLoading || isError) return []
-    return data.map((i) => ({ value: i.name }))
+    return data.map((bundle) => ({
+      value: bundle.name,
+      label: bundle.name,
+      isProduction: bundle.isProduction,
+      isStaging: bundle.isStaging,
+    }))
   }, [data])
 
   return (
@@ -72,6 +114,7 @@ const BundleDropdown = ({ bundleName, setBundleName, disabled }) => {
       placeholder="Select a bundle"
       style={{ flexGrow: 1 }}
       disabled={disabled}
+      itemTemplate={formatBundleDropdownItem}
     />
   )
 }
