b950bc8182a78d8da2c103af067afa5468ef7816
Martin Wacker <martas@imm.cz>
2023-10-02 18:04:49 +0200

    variant selection dropdown for dev mode (prototype)

--- src/containers/AddonSettings/VariantSelector.jsx
+++ src/containers/AddonSettings/VariantSelector.jsx
@@ -1,14 +1,35 @@
-import { Button } from '@ynput/ayon-react-components'
+import { Button, Dropdown } from '@ynput/ayon-react-components'
 import { useSelector } from 'react-redux'
+import { useMemo } from 'react'
+
+import { useGetBundleListQuery } from '/src/services/bundles'
 
 const DevModeSelector = ({ variant, setVariant, disabled }) => {
-  return JSON.stringify({ variant, setVariant, disabled })
+  const { data: bundleList } = useGetBundleListQuery({})
+
+  const bundleOptions = useMemo(() => {
+    console.log('BUNDLE LIST', bundleList)
+    if (!bundleList?.length) return []
+    const bundles = bundleList.filter((b) => !b?.isArchived && b?.isDev)
+    return bundles.map((b) => ({ label: b.name, value: b.name }))
+  }, [bundleList])
+
+  return (
+    <Dropdown
+      options={bundleOptions}
+      value={[variant]}
+      onChange={(e) => setVariant(e[0])}
+      disabled={disabled}
+      style={{ flex: 1 }}
+    />
+  )
 }
 
 const VariantSelector = ({ variant, setVariant, disabled }) => {
   const user = useSelector((state) => state.user)
 
-  if (user.attrib.developerMode) return DevModeSelector({ variant, setVariant, disabled })
+  if (user.attrib.developerMode)
+    return <DevModeSelector variant={variant} setVariant={setVariant} disabled={disabled} />
 
   const styleHlProd = {
     backgroundColor: 'var(--color-hl-production)',
