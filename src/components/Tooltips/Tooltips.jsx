bc44bac55cd68fa7019fad95c81bf7a618a3ba11
Innders <49156310+Innders@users.noreply.github.com>
2023-10-11 13:55:25 +0100

    chore: use one state

--- src/components/Tooltips/Tooltips.jsx
+++ src/components/Tooltips/Tooltips.jsx
@@ -5,9 +5,9 @@ import { isEqual } from 'lodash'
 const Tooltips = ({ render }) => {
   const tooltipRef = useRef(null)
   const [tooltip, setTooltip] = useState(null)
-  const [targetPos, setTargetPos] = useState(null)
 
   const getTooltipPos = (target, ref) => {
+    if (!target || !ref.current) return
     const tooltipRect = ref.current.getBoundingClientRect()
     const tooltipWidth = tooltipRect.width
     const tooltipHeight = tooltipRect.height
@@ -29,35 +29,35 @@ const Tooltips = ({ render }) => {
     return { x: tooltipLeft, y: tooltipTop }
   }
 
-  const [tooltipPos, setTooltipPos] = useState(null)
   const [timeoutId, setTimeoutId] = useState(null)
   const [isActive, setIsActive] = useState(false)
 
   // when tooltip changes, update tooltip position using target position
   useEffect(() => {
-    if (!tooltip || !targetPos) {
+    if (!tooltip) {
       // if tooltipPos is set, reset it
-      if (tooltipPos) setTooltipPos(null)
       if (timeoutId && !tooltip) {
         clearTimeout(timeoutId)
       }
       return
     }
 
-    const newTooltipPos = getTooltipPos(targetPos, tooltipRef)
+    const newTooltipPos = getTooltipPos(tooltip?.target, tooltipRef)
 
-    if (isActive && isEqual(tooltipPos, newTooltipPos)) return
+    if (isActive && isEqual(tooltip?.pos, newTooltipPos)) return
     // update state
     if (timeoutId) clearTimeout(timeoutId)
 
     if (isActive) {
-      setTooltipPos(newTooltipPos)
+      setTooltip((t) => ({ ...t, pos: newTooltipPos, posSet: true }))
       setTimeoutId(null)
     } else {
       setIsActive(true)
-      setTimeoutId(setTimeout(() => setTooltipPos(newTooltipPos), 350))
+      setTimeoutId(
+        setTimeout(() => setTooltip((t) => ({ ...t, pos: newTooltipPos, posSet: true })), 350),
+      )
     }
-  }, [tooltip, targetPos, tooltipPos])
+  }, [tooltip])
 
   // once tooltip has been null for 350ms, set noTimeOut to false
   const [activeTimeoutId, setActiveTimeoutId] = useState(null)
@@ -75,7 +75,6 @@ const Tooltips = ({ render }) => {
     if (!target) {
       if (tooltip) {
         setTooltip(null)
-        setTargetPos(null)
       }
       return
     }
@@ -84,9 +83,7 @@ const Tooltips = ({ render }) => {
     const shortcutData = target?.dataset?.shortcut
     // check if data-tooltip attribute exists
     if (!tooltipData && !shortcutData) {
-      setTargetPos(null)
       setTooltip(null)
-      setTargetPos(null)
       return
     }
 
@@ -97,22 +94,21 @@ const Tooltips = ({ render }) => {
     // target top will also be tooltip bottom
     const targetTop = target.getBoundingClientRect().top
 
+    const newTargetPos = { x: targetCenter, y: targetTop }
     const newTooltip = {
       tooltip: tooltipData ?? '',
       shortcut: shortcutData ?? '',
+      target: newTargetPos,
+      posSet: false,
     }
 
-    const newTargetPos = { x: targetCenter, y: targetTop }
-
     // check if tooltip is already set to same value
-    if (isEqual(tooltip, newTooltip) && isEqual(targetPos, newTargetPos)) return
+    if (isEqual(tooltip, newTooltip) && isEqual(tooltip?.target, newTargetPos)) return
 
-    setTargetPos(null)
     setTooltip(newTooltip)
-    setTargetPos(newTargetPos)
   }
 
-  console.log(tooltip, tooltipPos)
+  console.log(tooltip)
 
   return (
     <>
@@ -126,13 +122,13 @@ const Tooltips = ({ render }) => {
         className={tooltip?.tooltip ? 'tooltip' : 'tooltip hidden'}
         style={{
           display: tooltip ? 'flex' : 'none',
-          visibility: tooltipPos ? 'visible' : 'hidden',
-          opacity: tooltipPos ? 1 : 0,
-          left: tooltipPos?.x || 0,
-          top: tooltipPos?.y || 0,
+          visibility: tooltip?.pos ? 'visible' : 'hidden',
+          opacity: tooltip?.pos ? 1 : 0,
+          left: tooltip?.pos?.x || 0,
+          top: tooltip?.pos?.y || 0,
           padding: tooltip?.tooltip ? '6px 8px' : 6,
         }}
-        $targetPos={targetPos}
+        $targetPos={tooltip?.target}
       >
         {tooltip?.tooltip}
         {tooltip?.shortcut && <Styled.Shortcut>{tooltip?.shortcut}</Styled.Shortcut>}
