63e8a91a21c0ecfeb552218a1c7279a43cc53690
Innders <49156310+Innders@users.noreply.github.com>
2023-12-12 17:01:02 +0000

    fix: copy to clipboard fallback

--- src/helpers/copyToClipboard.js
+++ src/helpers/copyToClipboard.js
@@ -1,13 +1,41 @@
 import { toast } from 'react-toastify'
 
 const copyToClipboard = (message) => {
-  //  Check if the message is empty
   if (!message) return
-  //  Check if the browser supports the clipboard API
-  if (!navigator.clipboard) return
 
-  navigator.clipboard.writeText(message)
-  toast.success(`Copied To Clipboard: "${message}"`)
+  try {
+    if (navigator.clipboard) {
+      navigator.clipboard
+        .writeText(message)
+        .then(() => toast.success(`Copied To Clipboard: "${message}"`))
+        .catch((err) => {
+          console.error('Could not copy text: ', err)
+          toast.error('Could not copy text')
+        })
+    } else {
+      fallbackCopyTextToClipboard(message)
+    }
+  } catch (error) {
+    console.error('Unexpected error: ', error)
+  }
+}
+
+const fallbackCopyTextToClipboard = (text) => {
+  let textarea = document.createElement('textarea')
+  textarea.textContent = text
+  textarea.style.position = 'fixed' // Prevent scrolling to bottom of page in MS Edge.
+  document.body.appendChild(textarea)
+  textarea.select()
+
+  try {
+    document.execCommand('copy') // Security exception may be thrown by some browsers.
+    toast.success(`Copied To Clipboard: "${text}"`)
+  } catch (ex) {
+    console.warn('Copy to clipboard failed.', ex)
+    toast.error('Could not copy text')
+  } finally {
+    document.body.removeChild(textarea)
+  }
 }
 
 export default copyToClipboard
