1970efd13e1b273cc73258c2e916eb060100be22
Martin Wacker <martas@imm.cz>
2024-01-29 16:39:22 +0100

    refactored anatomy editor, use in project page, support for breadcrumbs

--- src/containers/AnatomyEditor.jsx
+++ src/containers/AnatomyEditor.jsx
@@ -1,12 +1,25 @@
+/*
+ * AnatomyEditor is a wrapper around SettingsEditor specifically for anatomy settings.
+ * It loads anatomy schema and displays the editor, it also handles loading anatomy:
+ * either a preset or existing project (one of projectName or preset must be provided)
+ *
+ * It however does not handle saving anatomy, this is done by the parent component.
+ * Along with simple editing, it also handles copy+paste of anatomy settings,
+ * and dispatching breadcrumbs to the store.
+ */
+
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
 import {
   useGetAnatomyPresetQuery,
   useGetAnatomySchemaQuery,
 } from '/src/services/anatomy/getAnatomy'
 
+import { useGetProjectAnatomyQuery } from '/src/services/project/getProject'
 import { isEqual } from 'lodash'
 
+import { setUri } from '/src/features/context'
 import SettingsEditor from '/src/containers/SettingsEditor'
 import {
   getValueByPath,
@@ -18,6 +31,7 @@ import pasteFromClipboard from '/src/helpers/pasteFromClipboard'
 
 const AnatomyEditor = ({
   preset,
+  projectName,
   formData,
   setFormData,
   breadcrumbs,
@@ -28,14 +42,24 @@ const AnatomyEditor = ({
 
   const { data: schema } = useGetAnatomySchemaQuery()
 
-  const { data: anatomyData, isSuccess } = useGetAnatomyPresetQuery({ preset }, { skip: !preset })
+  const { data: anatomyPresetData } = useGetAnatomyPresetQuery({ preset }, { skip: !preset })
+  const { data: projectAnatomyData } = useGetProjectAnatomyQuery(
+    { projectName },
+    { skip: !projectName },
+  )
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    if ((isSuccess, anatomyData)) {
-      setFormData(anatomyData)
-      setOriginalData(anatomyData)
-    }
-  }, [preset, isSuccess, anatomyData])
+    if (!anatomyPresetData) return
+    setFormData(anatomyPresetData)
+    setOriginalData(anatomyPresetData)
+  }, [anatomyPresetData])
+
+  useEffect(() => {
+    if (!projectAnatomyData) return
+    setFormData(projectAnatomyData)
+    setOriginalData(projectAnatomyData)
+  }, [projectAnatomyData])
 
   useEffect(() => {
     if (!setIsChanged) return
@@ -65,7 +89,17 @@ const AnatomyEditor = ({
     }
   }
 
+  if (!(preset || projectName)) return 'No preset or project selected'
+  if (preset && projectName) return 'Select either preset or project'
   if (!(schema && originalData)) return null
+
+  const handleBreadcrumbs = (path) => {
+    let uri = projectName ? `ayon+anatomy://${projectName}/` : `ayon+anatomy+preset://${preset}/`
+    uri += path.join('/')
+    dispatch(setUri(uri))
+
+    if (setBreadcrumbs) setBreadcrumbs(path)
+  }
 
   return (
     <SettingsEditor
@@ -73,7 +107,7 @@ const AnatomyEditor = ({
       originalData={originalData}
       formData={formData}
       onChange={setFormData}
-      onSetBreadcrumbs={setBreadcrumbs}
+      onSetBreadcrumbs={handleBreadcrumbs}
       breadcrumbs={breadcrumbs}
       context={{
         onPasteValue: onPasteValue,
