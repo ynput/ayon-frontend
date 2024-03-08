b9a5896e577a386f728e0e7102650cd417128040
Martin Wacker <martas@imm.cz>
2024-03-08 10:12:10 +0100

    persistent preferred site

--- src/containers/SiteDropdown.jsx
+++ src/containers/SiteDropdown.jsx
@@ -1,20 +1,34 @@
 import ayonClient from '/src/ayon'
-import { useMemo } from 'react'
+import { useMemo, useEffect } from 'react'
 import { Dropdown } from '@ynput/ayon-react-components'
+import useLocalStorage from '/src/hooks/useLocalStorage'
 
 const SiteDropdown = ({ value, onChange, disabled, multiselect = false, allowNull = false }) => {
+  const [preferredSite, setPreferredSite] = useLocalStorage('prefferedSite', null)
+
   const siteOptions = useMemo(() => {
     const sites = []
     sites.push(...ayonClient.settings.sites.map((site) => ({ value: site.id, label: site.id })))
     return sites
   }, [])
 
+  useEffect(() => {
+    if (!value && preferredSite && siteOptions.find((site) => site.value === preferredSite))
+      onChange(multiselect ? [preferredSite] : preferredSite)
+  }, [preferredSite])
+
+  const handleChange = (e) => {
+    const val = multiselect ? e : e[0]
+    setPreferredSite(val)
+    onChange(val)
+  }
+
   return (
     <Dropdown
       value={value ? [value] : null}
       options={siteOptions}
       multiSelect={multiselect}
-      onChange={(e) => (multiselect ? onChange(e) : onChange(e[0]))}
+      onChange={handleChange}
       onClear={() => (allowNull ? onChange(null) : undefined)}
       onClearNullValue={allowNull}
       placeholder="Select a site"
