1cc580e9cbfad4f9b3dac66b63127cf0c0b9b21e
Luke Inderwick <49156310+Innders@users.noreply.github.com>
2024-03-29 12:14:19 +0000

    fix regex
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- src/helpers/checkName.js
+++ src/helpers/checkName.js
@@ -3,7 +3,7 @@ import { snakeCase } from 'lodash'
 const checkName = (name) => {
   let newName = name
   // check name matches regex
-  const regex = /^[a-zA-Z_][a-zA-Z_]*[a-zA-Z_]$/
+  const regex = /^[a-zA-Z0-9_]+$/
   // if not, convert to snake_case
   if (name && !name.match(regex)) {
     newName = snakeCase(name)
