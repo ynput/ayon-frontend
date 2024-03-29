235edf91ca4aeae0af4e02d3434ee7cf328bdf41
Innders <49156310+Innders@users.noreply.github.com>
2024-03-29 10:36:14 +0000

    fix: sorting tasks by date

--- src/pages/UserDashboardPage/util/getSortedTasks.js
+++ src/pages/UserDashboardPage/util/getSortedTasks.js
@@ -5,11 +5,23 @@ export const getSortedTasks = (tasks = [], sortBy = []) => {
   return [...tasks].sort((a, b) => {
     for (let i = 0; i < sortBy.length; i++) {
       const { id, sortOrder } = sortBy[i]
-      if (a[id] < b[id]) {
-        return sortOrder ? -1 : 1
-      }
-      if (a[id] > b[id]) {
-        return sortOrder ? 1 : -1
+      if (id.toLowerCase().includes('date')) {
+        const dateA = new Date(b[id])
+        const dateB = new Date(a[id])
+
+        if (dateA < dateB) {
+          return sortOrder ? -1 : 1
+        }
+        if (dateA > dateB) {
+          return sortOrder ? 1 : -1
+        }
+      } else {
+        if (a[id] < b[id]) {
+          return sortOrder ? -1 : 1
+        }
+        if (a[id] > b[id]) {
+          return sortOrder ? 1 : -1
+        }
       }
     }
     return 0
