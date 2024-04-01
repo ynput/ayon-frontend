21f5ab681e83a789e9188a95aac45dd143f587e4
Innders <49156310+Innders@users.noreply.github.com>
2024-04-01 13:58:20 +0100

    fix: notification dot styles placement

--- src/containers/header/HeaderButton.jsx
+++ src/containers/header/HeaderButton.jsx
@@ -37,32 +37,33 @@ const HeaderButton = styled(Button)`
       outline: solid 1px var(--md-sys-color-outline-variant);
       background-color: var(--md-sys-color-background);
     `} {
-    &::after {
-      content: '';
-      position: absolute;
-      right: -4px;
-      top: -4px;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-      background-color: var(--md-sys-color-error);
-      user-select: none;
-      pointer-events: none;
+  }
 
-      /* closed */
-      opacity: 0;
-      scale: 0;
+  &::after {
+    content: '';
+    position: absolute;
+    right: -4px;
+    top: -4px;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: var(--md-sys-color-error);
+    user-select: none;
+    pointer-events: none;
 
-      transition: opacity 0.1s, scale 0.1s;
-      transition-delay: 0.5s;
-      transition-timing-function: cubic-bezier(0, 0.67, 0.48, 1.47);
-    }
+    /* closed */
+    opacity: 0;
+    scale: 0;
 
-    /* open */
-    &.notification::after {
-      opacity: 1;
-      scale: 1;
-    }
+    transition: opacity 0.1s, scale 0.1s;
+    transition-delay: 0.5s;
+    transition-timing-function: cubic-bezier(0, 0.67, 0.48, 1.47);
+  }
+
+  /* open */
+  &.notification::after {
+    opacity: 1;
+    scale: 1;
   }
 `
 
