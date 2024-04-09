909ad0f2b48087202783aa2b5156b41404593340
Innders <49156310+Innders@users.noreply.github.com>
2024-04-09 16:40:03 +0100

    fix: comment tooltip portal

--- src/components/Feed/FeedReferencePopup/FeedReferencePopup.jsx
+++ src/components/Feed/FeedReferencePopup/FeedReferencePopup.jsx
@@ -1,11 +1,12 @@
 import { UserImage } from '@ynput/ayon-react-components'
 import * as Styled from './FeedReferencePopup.styled'
 import Thumbnail from '/src/containers/thumbnail'
+import { createPortal } from 'react-dom'
 
 const FeedReferencePopup = ({ type, label, pos = {} }) => {
   // TODO: get data from id and type
   // TODO: See if label contain fullName or name and passed props down correcty in UserImage
-  return (
+  return createPortal(
     <Styled.Popup style={pos || {}}>
       {type === 'user' ? (
         <UserImage src={''} name={label} />
@@ -16,7 +17,8 @@ const FeedReferencePopup = ({ type, label, pos = {} }) => {
         <span>{type === 'user' ? label : 'ShotName'}</span>
         <span className={'label'}>{type === 'user' ? 'user name' : label}</span>
       </Styled.Content>
-    </Styled.Popup>
+    </Styled.Popup>,
+    document.body,
   )
 }
 
