83010c07a91934b6dce708ee17a984d203cb9a4b
Martin Wacker <martas@imm.cz>
2024-04-10 10:38:27 +0200

    added placeholders for other actitvity types

--- src/components/Feed/ActivityItem.jsx
+++ src/components/Feed/ActivityItem.jsx
@@ -5,6 +5,12 @@ const ActivityItem = ({ users, activity, entityType }) => {
   switch (activity.activityType) {
     case 'comment':
       return <ActivityComment activity={activity} users={users} entityType={entityType} />
+    case 'status.change':
+      return <ActivityComment activity={activity} users={users} entityType={entityType} />
+    case 'assignee.add':
+      return <ActivityComment activity={activity} users={users} entityType={entityType} />
+    case 'assignee.remove':
+      return <ActivityComment activity={activity} users={users} entityType={entityType} />
 
     default:
       return null
