608b468873168dc7dec0627d93daf4ab3291c48f
Innders <49156310+Innders@users.noreply.github.com>
2024-04-10 13:21:09 +0100

    chore: kanban sort by options file

---
+++ src/pages/UserDashboardPage/UserDashboardTasks/DashboardTasksToolbar/KanBanSortByOptions.js
@@ -0,0 +1,8 @@
+const sortByOptions = [
+  { id: 'folderName', label: 'Folder', sortOrder: true },
+  { id: 'name', label: 'Task', sortOrder: true },
+  { id: 'status', label: 'Status', sortOrder: true },
+  { id: 'endDate', label: 'Due Date', sortOrder: true },
+]
+
+export default sortByOptions
