9935b4cb466b6c68fe1ad1e4f342ffee89cb0568
Innders <49156310+Innders@users.noreply.github.com>
2024-05-08 12:23:48 +0100

    fix(actions): better placeholder

--- src/components/Actions/Actions.jsx
+++ src/components/Actions/Actions.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import * as Styled from './Actions.styled'
+import { Button } from '@ynput/ayon-react-components'
 
 const Actions = ({ options = [], pinned = [] }) => {
   // options =[{id: 'nuke', icon: 'nuke.png'}]
   // pinned = ['nuke']
+
+  const showPlaceholder = true
+  // return empty placeholder until finished
+  if (showPlaceholder)
+    return <Button icon="manufacturing" disabled data-tooltip="Actions coming soon" />
 
   return (
     <Styled.Actions>
