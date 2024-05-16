ceef8259c54e7bbbebd1b26bdd3ef573df0f8bc1
Ludovit Holbik <ludovit.holbik@inventi.cz>
2024-05-16 18:51:19 +0200

    fix: linting fix

--- src/pages/TeamsPage/UsersListTeamsSmall.jsx
+++ src/pages/TeamsPage/UsersListTeamsSmall.jsx
@@ -5,70 +5,70 @@ import UserImage from '/src/components/UserImage'
 import styled from 'styled-components'
 
 const StyledProfileRow = styled.div`
-    display: flex;
-    align-items: center;
-    gap: var(--base-gap-large);
-    overflow: hidden;
+  display: flex;
+  align-items: center;
+  gap: var(--base-gap-large);
+  overflow: hidden;
 `
 
 const StyledUserImage = styled(UserImage)`
-    margin: 'auto';
-    transform: scale(0.8);
+  margin: 'auto';
+  transform: scale(0.8);
 `
 
 const StyledSection = styled(Section)`
-    height: 85%;
-    flex: 1.5;
+  height: 85%;
+  flex: 1.5;
 `
 
 const FullnameImage = ({ rowData }) => {
-    const { name, self, isMissing } = rowData
-    const { fullName } = rowData.attrib || false
-    return (
-      <StyledProfileRow>
-        <StyledUserImage $isMissing={isMissing} name={name} size={25} highlight={self}/>
-        <span style={{ color: isMissing ? 'var(--color-hl-error)' : 'inherit' }}>
-          {fullName || name}
-        </span>
-      </StyledProfileRow>
-    )
+  const { name, self, isMissing } = rowData
+  const { fullName } = rowData.attrib || false
+  return (
+    <StyledProfileRow>
+      <StyledUserImage $isMissing={isMissing} name={name} size={25} highlight={self}/>
+      <span style={{ color: isMissing ? 'var(--color-hl-error)' : 'inherit' }}>
+        {fullName || name}
+      </span>
+    </StyledProfileRow>
+  )
   }
 
 const UsersListTeamsSmall = ({ handleContext, userList, isLoading, onSelectionChange, onContextSelectionChange, selection }) => {
   return (
     <StyledSection>
-        <TablePanel onContextMenu={handleContext}>
-            <DataTable
-            value={userList}
-            scrollable="true"
-            scrollHeight="flex"
-            dataKey="name"
-            selectionMode="multiple"
-            className={`user-list-table ${isLoading ? 'table-loading' : ''}`}
-            onSelectionChange={onSelectionChange}
-            onContextMenuSelectionChange={onContextSelectionChange}
-            onContextMenu={handleContext}
-            selection={selection}
-            resizableColumns
-            responsive="true"
-            autoLayout="true"
-            tableStyle={{
-                width: '100%',
-            }}
-            groupRowsBy={'group'}
-            rowGroupMode="subheader"
-            rowGroupHeaderTemplate={(data) => {
-                return <div>{data.group}</div>
-            }}
-            >
-                <Column
-                field="attrib.fullName"
-                header="Full Name"
-                style={{ width: '20%' }}
-                body={(rowData) => FullnameImage({ rowData })}
-                />
-            </DataTable>
-        </TablePanel>
+      <TablePanel onContextMenu={handleContext}>
+        <DataTable
+          value={userList}
+          scrollable="true"
+          scrollHeight="flex"
+          dataKey="name"
+          selectionMode="multiple"
+          className={`user-list-table ${isLoading ? 'table-loading' : ''}`}
+          onSelectionChange={onSelectionChange}
+          onContextMenuSelectionChange={onContextSelectionChange}
+          onContextMenu={handleContext}
+          selection={selection}
+          resizableColumns
+          responsive="true"
+          autoLayout="true"
+          tableStyle={{
+              width: '100%',
+          }}
+          groupRowsBy={'group'}
+          rowGroupMode="subheader"
+          rowGroupHeaderTemplate={(data) => {
+              return <div>{data.group}</div>
+        }}
+        >
+          <Column
+            field="attrib.fullName"
+            header="Full Name"
+            style={{ width: '20%' }}
+            body={(rowData) => FullnameImage({ rowData })}
+          />
+        </DataTable>
+      </TablePanel>
     </StyledSection>
   )
 }
