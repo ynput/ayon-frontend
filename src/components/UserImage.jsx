1474aa8f4ece80f3aa4d9d81341550e554abf176
Ludovit Holbik <ludovit.holbik@inventi.cz>
2024-05-16 12:20:32 +0200

    enhacement: added shimmer effect for missing image in UserImage

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,8 +1,22 @@
-import { UserImage as UserImageArc } from '@ynput/ayon-react-components'
+import { UserImage as UserImageArc, getShimmerStyles } from '@ynput/ayon-react-components'
+import styled, { css } from 'styled-components'
 
+const UserImageArcPlaceholder = styled(UserImageArc)`
+     position: relative;
+     > span {
+      display: none;
+     }
+     ${({ $name }) => $name &&
+      css`
+          color: transparent;
+          border-radius: medium;
+          ${getShimmerStyles()}
+      `}
+`
 // wraps the ARC UserImage component to use new user image api
 const UserImage = ({ name, imageKey, ...props }) => {
-  return <UserImageArc name={name} src={name && `/api/users/${name}/avatar${imageKey || ''}`} {...props} />
+  if (name) return  <UserImageArcPlaceholder $name={name} {...props} />
+  return <UserImageArc name={name} src={`/api/users/${name}/avatar${imageKey || ''}`} {...props} />
 }
 
 export default UserImage
