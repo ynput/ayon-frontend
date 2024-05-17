5956367db3b4f65c38174c08600cc1b46042b74c
Innders <49156310+Innders@users.noreply.github.com>
2024-05-17 14:50:06 +0100

    chore(tests): fix auth setup

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -2,13 +2,19 @@ import { test as setup } from '@playwright/test'
 
 const authFile = 'playwright/.auth/user.json'
 
-setup('authenticate', async ({ request }) => {
-  // Send authentication request. Replace with your own.
-  await request.post('/api/auth/login', {
-    form: {
-      name: process.env.USER_NAME,
-      password: process.env.PASSWORD,
-    },
-  })
-  await request.storageState({ path: authFile })
+setup('authenticate', async ({ page }) => {
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto('/login')
+  await page.getByLabel('Username').fill(process.env.USER_NAME)
+  await page.getByLabel('Password').fill(process.env.PASSWORD)
+  await page.getByRole('button', { name: 'Login', exact: true }).click()
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set.
+  await page.waitForURL('/')
+
+  // End of authentication steps.
+
+  await page.context().storageState({ path: authFile })
 })
