557247f5b3fb506df817aa5453da3b60ffbaa265
Ludovit Holbik <ludovit.holbik@inventi.cz>
2024-05-20 15:54:22 +0200

    fix(addons): addon card prioritize addon title then name

--- src/components/AddonCard/AddonCard.jsx
+++ src/components/AddonCard/AddonCard.jsx
@@ -4,12 +4,11 @@ import { Icon } from '@ynput/ayon-react-components'
 import Type from '/src/theme/typography.module.css'
 
 const AddonCard = React.forwardRef(
-  ({ name, icon = 'check_circle', isSelected, error, version, ...props }, ref) => {
-    console.log(version,'version')
+  ({ name, icon = 'check_circle', isSelected, error, version, title, ...props }, ref) => {
     return (
       <Styled.AddonCard ref={ref} $selected={isSelected} $error={!!error} {...props}>
         <Icon icon={icon} />
-        <span className={Type.titleSmall}>{name}</span>
+        <span className={Type.titleSmall}>{title || name}</span>
         {error && <span className="error">{error}</span>}
         {version && <span className="error">{version}</span>}
       </Styled.AddonCard>
