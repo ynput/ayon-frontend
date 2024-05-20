8059b079bbd4edb39eae5ca07cc42f4f1a50d966
Innders <49156310+Innders@users.noreply.github.com>
2024-05-20 13:39:56 +0100

    chore(preview): set up preview dialog architecture

---
+++ src/components/VersionSelectorTool/VersionSelectorTool.jsx
@@ -0,0 +1,7 @@
+import * as Styled from './VersionSelectorTool.styled'
+
+const VersionSelectorTool = ({ versions, selected, latest, approved, hero, onChange }) => {
+  return <Styled.Tools>VersionSelectorTool</Styled.Tools>
+}
+
+export default VersionSelectorTool
