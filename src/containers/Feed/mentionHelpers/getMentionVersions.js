83f23aea890b9914b7a04ece3b21db3f87fca9b7
Innders <49156310+Innders@users.noreply.github.com>
2024-05-31 15:10:40 +0100

    fix(mentions): get correct version product icon

--- src/containers/Feed/mentionHelpers/getMentionVersions.js
+++ src/containers/Feed/mentionHelpers/getMentionVersions.js
@@ -3,20 +3,22 @@ import { productTypes } from '/src/features/project'
 import getEntityTypeIcon from '/src/helpers/getEntityTypeIcon'
 
 const getMentionVersions = (versions) => {
-  const productType = productTypes[versions[0]?.product?.productType]
-  const icon = productType?.icon || getEntityTypeIcon('version')
+  return versions.map((v) => {
+    const productType = productTypes[v?.product?.productType]
+    const icon = productType?.icon || getEntityTypeIcon('version')
 
-  return versions.map((v) => ({
-    type: 'version',
-    label: v.name,
-    image: v.thumbnailId,
-    icon: icon,
-    id: v.id,
-    createdAt: v.createdAt,
-    context: v.product?.name,
-    keywords: [v.name, v.product?.name],
-    suffix: getFuzzyDate(v.createdAt),
-  }))
+    return {
+      type: 'version',
+      label: v.name,
+      image: v.thumbnailId,
+      icon: icon,
+      id: v.id,
+      createdAt: v.createdAt,
+      context: v.product?.name,
+      keywords: [v.name, v.product?.name],
+      suffix: getFuzzyDate(v.createdAt),
+    }
+  })
 }
 
 export default getMentionVersions
