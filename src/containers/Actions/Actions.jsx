eb42d81ae01195596c991c62a1698c4a84705fbe
Martin Wacker <martas@imm.cz>
2024-06-24 18:08:20 +0200

    feat: execute void and launcher actions

--- src/containers/Actions/Actions.jsx
+++ src/containers/Actions/Actions.jsx
@@ -2,52 +2,86 @@ import React from 'react'
 import * as Styled from './Actions.styled'
 import { Button } from '@ynput/ayon-react-components'
 import { classNames } from 'primereact/utils'
+import { toast } from 'react-toastify'
 
-
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useMemo} from 'react'
 import axios from 'axios'
 
 
 const Actions = ({ entities }) => {
   const [isLoading, setIsLoading] = useState(true)
-  const [bundleVariant, setBundleVariant] = useState(null)
+  const [bundleVariant, setBundleVariant] = useState("production")
   const [options, setOptions] = useState([])
   
-  console.log("Entities for actions", entities)
+  const context = useMemo(() => {
+    if (!entities.length) return null
+    if (!entities[0].projectName) return null
+    if (!entities[0].entityType) return null
 
+    // get a list of unique entity subtypes
+    const entitySubtypes = entities.map((entity) => entity.entitySubType)
+      .filter((value, index, self) => self.indexOf(value) === index)
 
-  useEffect(() => {
-    if (!entities.length || !entities[0].projectName || !entities[0].entityType){
-      setOptions([])
-      return
-    }
-
-    setIsLoading(true)
-
-    const payload = {
+    return {
       projectName: entities[0].projectName,
       entityType: entities[0].entityType,
       entityIds: entities.map((entity) => entity.id),
+      entitySubtypes,
     }
-      
+  }, [entities])
 
+
+  useEffect(() => {
+    if (!context) {
+      setIsLoading(false)
+      setOptions([])
+      return
+    }
+    setIsLoading(true)
+      
     axios
-      .post('/api/actions/list', payload)
+      .post('/api/actions/list', context)
       .then((response) => {
         setOptions(response.data.actions)
         setBundleVariant(response.data.bundleVariant)
+      })
+      .catch((error) => {
+        console.warn("Error fetching actions", error)
+        setOptions([])
       })
       .finally(() => {
         setIsLoading(false)
       })
 
-
-  }, [entities])
+  }, [context])
 
 
   const onExecuteAction = (identifier) => {
     const action = options.find((option) => option.identifier === identifier)
     console.log("Executing action", action)
+
+    const params = {
+      addonName: action.addonName,
+      addonVersion: action.addonVersion,
+      variant: bundleVariant,
+      identifier: action.identifier,
+    }
+
+    axios
+      .post( '/api/actions/execute', context, { params })
+      .then((response) => {
+        console.log("Action executed", response)
+        toast.success(response?.data?.message || "Action executed successfully")
+        if (response?.data?.uri) {
+          window.location.href = response.data.uri
+        }
+      })
+      .catch((error) => {
+        console.warn("Error executing action", error)
+        toast.error(error?.response?.data?.message || "Error executing action")
+      
+      })
+
   }
 
 
