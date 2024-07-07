9bfeaa81bbefdf4f8c03920e4c6bf84e80cd7190
Innders <49156310+Innders@users.noreply.github.com>
2024-07-07 15:14:20 +0100

    feat(review): thumbnail hover tooltips

--- src/components/ReviewablesSelector/ReviewablesSelector.styled.ts
+++ src/components/ReviewablesSelector/ReviewablesSelector.styled.ts
@@ -1,25 +1,30 @@
 import styled from 'styled-components'
 
 export const ReviewablesSelector = styled.div`
+  position: relative;
+  height: 100%;
+
+  z-index: 200;
+`
+
+export const Scrollable = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   overflow-y: auto;
   min-width: fit-content;
-  height: 100%;
-
   gap: var(--base-gap-small);
 `
 
 export const ReviewableCard = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 89px;
 
   border-radius: var(--border-radius-l);
-  overflow: hidden;
   background-color: var(--md-sys-color-surface-container-high);
   border: solid 2px var(--md-sys-color-surface-container-high);
   padding: 2px;
@@ -53,4 +58,16 @@ export const ReviewableCard = styled.div`
     border-color: var(--md-sys-color-primary);
     color: var(--md-sys-color-on-primary-container);
   }
+`
+
+export const Label = styled.span`
+  padding: var(--padding-m);
+  border-radius: var(--border-radius-m);
+  background-color: var(--md-sys-color-surface-container-low);
+  border: 1px solid var(--md-sys-color-outline);
+  min-width: max-content;
+
+  position: absolute;
+  left: -8px;
+  transform: translateX(-100%) translateY(-50%);
 `
--- src/components/ReviewablesSelector/ReviewablesSelector.tsx
+++ src/components/ReviewablesSelector/ReviewablesSelector.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import * as Styled from './ReviewablesSelector.styled'
-import Typography from '@/theme/typography.module.css'
 import { classNames } from 'primereact/utils'
 
 type ReviewableCard = {
@@ -21,6 +20,8 @@ const ReviewablesSelector: FC<ReviewablesSelectorProps> = ({
   selected = [],
   onChange,
 }) => {
+  const [labelTooltip, setLabelTooltip] = useState<null | string>(null)
+  const [labelTooltipYPos, setLabelTooltipYPos] = useState<null | number>(null)
   // add keyboard support
   // use up and down arrow keys to navigate through the reviewables
   // if at top and press up, go to bottom, if at bottom and press down, go to top
@@ -33,11 +34,21 @@ const ReviewablesSelector: FC<ReviewablesSelectorProps> = ({
           selected.includes(activityId),
         )
         const nextIndex = e.key === 'ArrowUp' ? currentIndex - 1 : currentIndex + 1
-        const nextActivityId =
+        const nextReviewable =
           reviewables[nextIndex < 0 ? reviewables.length - 1 : nextIndex % reviewables.length]
-            .activityId
+        const nextActivityId = nextReviewable.activityId
 
         onChange && onChange(nextActivityId)
+        // also set new label for the tooltip
+        setLabelTooltip(nextReviewable.label)
+        // set label tooltip position
+        const el = document.getElementById('preview-' + nextActivityId)
+
+        if (el) {
+          console.log(el)
+          const top = el.offsetTop + el.offsetHeight / 2
+          setLabelTooltipYPos(top)
+        }
       }
     }
 
@@ -48,22 +59,54 @@ const ReviewablesSelector: FC<ReviewablesSelectorProps> = ({
     }
   }, [selected, reviewables, onChange])
 
-  // if only one return null
-  //   if (reviewables.length === 1) return null
+  // keep track of when NOT hovering over the reviewable cards
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      // check if the mouse is over a reviewable card
+      const closest = (event.target as HTMLElement).closest('.reviewables')
+      if (!closest) {
+        setLabelTooltip(null)
+      }
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+    }
+  }, [])
+
+  const handleMouseOver = (
+    event: React.MouseEvent<HTMLDivElement>,
+    { label }: Pick<ReviewableCard, 'label'>,
+  ) => {
+    // check event is coming from a reviewable card
+    const closest = (event.target as HTMLElement).closest('.reviewable-card') as HTMLElement
+    if (!closest) return
+    setLabelTooltip(label)
+    // set label tooltip position
+    const top = closest.offsetTop + closest.offsetHeight / 2
+    setLabelTooltipYPos(top)
+  }
 
   return (
     <Styled.ReviewablesSelector>
-      {reviewables.map(({ activityId, label }) => (
-        <Styled.ReviewableCard
-          key={activityId}
-          onClick={() => onChange && onChange(activityId)}
-          className={classNames({ selected: selected.includes(activityId) })}
-          data-tooltip={label}
-        >
-          <img src={'https://placehold.co/160x90'} />
-          {/* <span className={Typography.labelSmall}>{label}</span> */}
-        </Styled.ReviewableCard>
-      ))}
+      <Styled.Scrollable className="reviewables">
+        {reviewables.map(({ activityId, label }) => (
+          <Styled.ReviewableCard
+            key={activityId}
+            id={'preview-' + activityId}
+            onClick={() => onChange && onChange(activityId)}
+            className={classNames('reviewable-card', { selected: selected.includes(activityId) })}
+            onMouseOver={(e) => handleMouseOver(e, { label })}
+          >
+            <img src={'https://placehold.co/160x90'} />
+          </Styled.ReviewableCard>
+        ))}
+      </Styled.Scrollable>
+      {labelTooltip && labelTooltipYPos && (
+        <Styled.Label style={{ top: labelTooltipYPos }}>{labelTooltip}</Styled.Label>
+      )}
     </Styled.ReviewablesSelector>
   )
 }
