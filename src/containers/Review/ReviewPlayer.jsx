c93712b6f1196dee9d667e97b1f2958c98a9f036
Innders <49156310+Innders@users.noreply.github.com>
2024-07-07 13:03:31 +0100

    chore: rendered more hooks

--- src/containers/Review/ReviewPlayer.jsx
+++ src/containers/Review/ReviewPlayer.jsx
@@ -7,20 +7,20 @@ const ReviewPlayer = ({ projectName, reviewable }) => {
   const [frameRate, setFrameRate] = useState(null)
   const [aspectRatio, setAspectRatio] = useState(null)
 
+  useEffect(() => {
+    const mediaInfo = reviewable?.mediaInfo
+    if (!mediaInfo) return
+    const { frameRate, width, height } = mediaInfo
+    setFrameRate(frameRate)
+    setAspectRatio(width / height)
+  }, [reviewable])
+
   if (!reviewable)
     return (
       <ReviewPlayerWrapper>
         <EmptyPlaceholder icon="hide_image" message={'This version has no reviewable content.'} />
       </ReviewPlayerWrapper>
     )
-
-  const mediaInfo = reviewable.mediaInfo
-
-  useEffect(() => {
-    const { frameRate, width, height } = mediaInfo
-    setFrameRate(frameRate)
-    setAspectRatio(width / height)
-  }, [reviewable])
 
   const videoSrc = `/api/projects/${projectName}/files/${reviewable.fileId}`
 
