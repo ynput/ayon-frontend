e7cc6bd6f8176d7d0aeb90d0785c468d4f9a106f
Innders <49156310+Innders@users.noreply.github.com>
2024-07-31 07:58:46 +0100

    feat(path): hover over hidden

--- src/components/EntityPath/EntityPath.styled.ts
+++ src/components/EntityPath/EntityPath.styled.ts
@@ -27,13 +27,52 @@ export const Path = styled.div`
 `
 
 export const Segment = styled.span`
-  /* cursor: pointer; */
+  position: relative;
+  cursor: pointer;
   padding: 0px 2px;
   border-radius: var(--border-radius-m);
-  transition: all 0.2s;
+  transition: color 0.2s, background-color 0.2s;
 
   &:hover {
-    /* background-color: var(--md-sys-color-surface-container-highest-hover);
-    color: var(--md-sys-color-on-surface); */
+    background-color: var(--md-sys-color-surface-container-highest-hover);
+    color: var(--md-sys-color-on-surface);
+  }
+
+  &.more {
+    padding: 0px 6px;
+
+    &:hover {
+      background-color: var(--md-sys-color-surface-container-high);
+    }
+  }
+`
+
+export const MoreModal = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0px;
+  width: 100%;
+  min-width: fit-content;
+
+  z-index: 100;
+  padding-top: 4px;
+`
+
+export const MoreList = styled.ul`
+  background-color: var(--md-sys-color-surface-container-high);
+  border-radius: var(--border-radius-m);
+  box-shadow: var(--md-sys-shadow-elevation-4);
+  padding: var(--padding-s);
+  list-style: none;
+
+  overflow: hidden;
+`
+
+export const MoreItem = styled.li`
+  padding: var(--base-gap-small);
+  border-radius: var(--border-radius-m);
+
+  &:hover {
+    background-color: var(--md-sys-color-surface-container-highest-hover);
   }
 `
--- src/components/EntityPath/EntityPath.tsx
+++ src/components/EntityPath/EntityPath.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from 'react'
+import { FC, Fragment, useRef, useState } from 'react'
 import * as Styled from './EntityPath.styled'
 import { classNames } from 'primereact/utils'
 
@@ -15,18 +15,37 @@ interface EntityPathProps {
 }
 
 const EntityPath: FC<EntityPathProps> = ({ projectName, segments, isLoading }) => {
+  const moreRef = useRef<HTMLSpanElement>(null)
+  const [moreHover, setMoreHover] = useState(false)
+
   // Check if there are fewer than or equal to 3 segments
   const segmentsToShow = segments.length <= 3 ? segments : segments.slice(-3)
   const hiddenSegments = segments.length <= 3 ? [] : segments.slice(0, -3)
 
   return (
-    <Styled.Path className={classNames({ loading: isLoading })}>
+    <Styled.Path className={classNames({ loading: isLoading })} id="entity-path">
       <Styled.Segment>{projectName}</Styled.Segment>
 
       {!!hiddenSegments.length && (
         <>
           <span>/</span>
-          <Styled.Segment>...</Styled.Segment>
+          <Styled.Segment
+            className="more"
+            ref={moreRef}
+            onMouseOver={() => setMoreHover(true)}
+            onMouseOut={() => setMoreHover(false)}
+          >
+            ...
+            {moreHover && (
+              <Styled.MoreModal>
+                <Styled.MoreList>
+                  {hiddenSegments.map((segment) => (
+                    <Styled.MoreItem key={segment.id}>{segment.label}</Styled.MoreItem>
+                  ))}
+                </Styled.MoreList>
+              </Styled.MoreModal>
+            )}
+          </Styled.Segment>
         </>
       )}
 
