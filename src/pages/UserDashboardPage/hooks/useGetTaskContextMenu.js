ec0f21876deaace730de540ab73ec71ee3459940
Innders <49156310+Innders@users.noreply.github.com>
2024-08-06 16:05:21 +0100

    fix(kanban): open in viewer right click

--- src/pages/UserDashboardPage/hooks/useGetTaskContextMenu.js
+++ src/pages/UserDashboardPage/hooks/useGetTaskContextMenu.js
@@ -4,31 +4,40 @@ import { onTaskSelected } from '@state/dashboard'
 import { useSelector } from 'react-redux'
 import { useURIContext } from '@context/uriContext'
 import { getTaskRoute } from '@helpers/routes'
+import useOpenTaskInViewer from './useOpenTaskInViewer'
 
 export const useGetTaskContextMenu = (tasks, dispatch) => {
   // URI NAVIGATE ON RIGHT CLICK
   const { navigate: navigateToUri } = useURIContext()
   const selectedTasks = useSelector((state) => state.dashboard.tasks.selected)
 
-  const getContextMenuItems = (card) => {
+  const openTaskInViewer = useOpenTaskInViewer()
+
+  const getContextMenuItems = (task) => {
     return [
       {
-        label: 'Open in Browser',
-        command: () => navigateToUri(getTaskRoute(card)),
+        label: 'Open in viewer',
+        command: () => openTaskInViewer(task),
+        icon: 'play_circle',
+        shortcut: 'Spacebar',
+      },
+      {
+        label: 'Open in browser',
+        command: () => navigateToUri(getTaskRoute(task)),
         icon: 'open_in_new',
-        disabled: selectedTasks.includes(card.id) && selectedTasks.length > 1,
+        shortcut: 'Double click',
       },
       {
         label: 'Copy task ID',
-        command: () => copyToClipboard(card.id),
+        command: () => copyToClipboard(task.id),
         icon: 'content_copy',
-        disabled: selectedTasks.includes(card.id) && selectedTasks.length > 1,
+        disabled: selectedTasks.includes(task.id) && selectedTasks.length > 1,
       },
       {
         label: 'Copy latest version ID',
-        command: () => copyToClipboard(card.latestVersionId),
+        command: () => copyToClipboard(task.latestVersionId),
         icon: 'content_copy',
-        disabled: !card.latestVersionId || selectedTasks.length > 1,
+        disabled: !task.latestVersionId || selectedTasks.length > 1,
       },
     ]
   }
@@ -41,17 +50,17 @@ export const useGetTaskContextMenu = (tasks, dispatch) => {
     const taskId = el.closest('.card')
     if (!taskId) return
     // find card
-    const card = tasks.find((t) => t.id === taskId.id)
+    const selectedTask = tasks.find((t) => t.id === taskId.id)
 
-    if (!card) return
+    if (!selectedTask) return
 
     // if task not already selected, select it and remove all other selections
-    if (!selectedTasks.includes(card.id)) {
-      dispatch(onTaskSelected({ ids: [card.id], types: [card.taskType] }))
+    if (!selectedTasks.includes(selectedTask.id)) {
+      dispatch(onTaskSelected({ ids: [selectedTask.id], types: [selectedTask.taskType] }))
     }
 
     // get context model
-    const contextMenuItems = getContextMenuItems(card)
+    const contextMenuItems = getContextMenuItems(selectedTask)
     // show context menu
     showContextMenu(e, contextMenuItems)
   }
