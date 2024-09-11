2a11ae6866fafddad294b1c75bb51ddd66c801fb
Innders <49156310+Innders@users.noreply.github.com>
2024-09-11 09:49:43 +0100

    fix(progress): shorter status bars

--- src/containers/TasksProgress/components/TaskStatusBar/TaskStatusBar.styled.ts
+++ src/containers/TasksProgress/components/TaskStatusBar/TaskStatusBar.styled.ts
@@ -4,12 +4,15 @@ export const Cell = styled.div`
   padding: 4px;
   width: 100%;
   height: 100%;
+  display: flex;
+  align-items: center;
 `
 
 export const StatusBar = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
+  height: 50%;
+  cursor: pointer;
   border-radius: var(--border-radius-m);
   overflow: hidden;
   background-color: var(--md-sys-color-surface-container-high);
