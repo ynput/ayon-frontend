51478e00d62f92339fe54658939e882dce6214ea
Florin Tudor <florin@ynput.io>
2024-10-02 10:53:18 +0200

    feature(Status) Using scoped status data on Kanban board also - listing mode

--- src/hooks/useScopedStatuses.ts
+++ src/hooks/useScopedStatuses.ts
@@ -1,5 +1,5 @@
-import { $Any } from "@types"
-import { useSelector } from "react-redux"
+import { $Any } from '@types'
+import { useSelector } from 'react-redux'
 
 type EntityStatus = {
   name: string
@@ -13,11 +13,18 @@ const useScopedStatuses = (entityTypes: string[]) => {
   const { statuses } = useSelector((state: $Any) => state.project)
   let statusesList: EntityStatus[] = Object.values(statuses || {})
 
+  return filterProjectStatuses(statusesList, entityTypes)
+}
+
+const filterProjectStatuses = (statuses: EntityStatus[], entityTypes: string[]) => {
+  let statusesList: EntityStatus[] = Object.values(statuses || {})
+
   if (statusesList.length == 0 || statusesList[0].scope === undefined) {
     return statusesList
   }
 
-  return  statusesList.filter(el => entityTypes.every(type => el.scope!.includes(type)))
+  return statusesList.filter((el) => entityTypes.every((type) => el.scope!.includes(type)))
 }
 
+export { filterProjectStatuses }
 export default useScopedStatuses
