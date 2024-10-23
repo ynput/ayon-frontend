e6801a984059ca267a19a9d27db95fd641be5113
Martastain <martas@imm.cz>
2024-10-23 10:33:33 +0200

    fix: removed hiding disabled access groups panels when disabled (until it works properly)

--- src/pages/SettingsPage/AccessGroups/AccessGroupDetail.jsx
+++ src/pages/SettingsPage/AccessGroups/AccessGroupDetail.jsx
@@ -116,7 +116,7 @@ const AccessGroupDetail = ({ projectName, accessGroupName }) => {
           level={projectName ? 'project' : 'studio'}
           context={{
             headerProjectName: projectName,
-            hideDisabledGroups: true,
+            /* hideDisabledGroups: true,*/
           }}
         />
       </ScrollPanel>
