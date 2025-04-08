7c7fdae4edd846c227dd3970eac61650583e372a
Innders <49156310+Innders@users.noreply.github.com>
2025-04-08 10:19:40 +0100

    chore: remove old SearchFilter component

--- src/components/SearchFilter/buildFilterId.ts
+++
@@ -1,3 +0,0 @@
-import { uuid } from 'short-uuid'
-
-export default (name: string) => `${name}_${uuid()}`
--- src/components/SearchFilter/doesFilterExist.ts
+++
@@ -1,9 +0,0 @@
-import getFilterFromId from './getFilterFromId'
-import { Filter } from './types'
-
-const doesFilterExist = (filterId: string, filters: Filter[]) => {
-  const filterName = getFilterFromId(filterId)
-  return filters.some((filter) => getFilterFromId(filter.id) === filterName)
-}
-
-export default doesFilterExist
