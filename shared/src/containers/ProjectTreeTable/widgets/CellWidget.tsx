a2310be366435649770bd3639df6542ae170ad57
Innders <49156310+Innders@users.noreply.github.com>
2025-06-17 17:23:59 +0100

    Revert "fix(overview): Ensure focus stays on table cell when canceling edit"
    This reverts commit 1023f2b4279619cda6a7638237f94433e4aa0322.

--- shared/src/containers/ProjectTreeTable/widgets/CellWidget.tsx
+++ shared/src/containers/ProjectTreeTable/widgets/CellWidget.tsx
@@ -134,9 +134,6 @@ const EditorCellComponent: FC<EditorCellProps> = ({
 
   const handleCancel = () => {
     setEditingCellId(null)
-    // ensure the browser focus moves back to the parent <td>
-    const td = ref.current?.closest('td') as HTMLElement | null
-    if (td) td.focus()
   }
 
   const widget = useMemo(() => {
