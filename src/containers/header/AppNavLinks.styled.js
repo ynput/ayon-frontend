6be7a99f9c7bb7603f91ea9b9c4c73c51c1a5151
filipvnencak <vnencak.filip@gmail.com>
2025-08-18 11:29:19 +0200

    back to the version where the help button was pushed off the screen

--- src/containers/header/AppNavLinks.styled.js
+++ src/containers/header/AppNavLinks.styled.js
@@ -1,85 +1,72 @@
 import styled from 'styled-components'
 
 export const NavBar = styled.nav`
-  
+    padding-right: 8px;
+    border-bottom: 1px solid var(--md-sys-color-outline-variant);
     background-color: var(--panel-background);
-    position: relative;
-    display: flex;
-    margin-top: 1px;
-    
-    .scrollable-tabs {
-        flex: 1;
+    padding: 0 8px;
+
+    ul {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: var(--base-gap-small);
+        /* reset defaults */
+        list-style: none;
+        margin: 0;
+        padding: 0;
+
+        /* this hides the border of the navbar by putting the active tab border over it */
+        position: relative;
+        top: 1px;
+        margin-bottom: -1px;
+
+        /* overflow */
+        width: 100%;
         overflow-x: auto;
+        /* hide scroll bar */
         scrollbar-width: none;
         -ms-overflow-style: none;
         &::-webkit-scrollbar {
             display: none;
         }
-
-        ul {
-            border-bottom: 1px solid var(--md-sys-color-outline-variant);
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            gap: var(--base-gap-small);
-            list-style: none;
-            margin: 0;
-            padding: 0 8px;
-            position: relative;
-        }
-    }
-
-    .fixed-buttons {
-        display: flex;
-        align-items: center;
-        gap: var(--base-gap-small);
-        position: relative;
-        background-color: var(--panel-background);
-        border-bottom: 1px solid var(--md-sys-color-outline-variant);
-        z-index: 2;
-
-        @media (min-width: 1200px) {
-            position: static;
-            background-color: transparent;
-            padding-left: 0;
-        }
     }
 `
 
 export const NavItem = styled.li`
-  user-select: none;
-  /* NavLink */
-  a {
-    padding: 2px 4px;
-    padding-bottom: 3px;
-    display: block;
-    border: 1px solid transparent;
-    border-radius: 4px 4px 0 0;
-    margin-bottom: -1px;
+    user-select: none;
+    /* NavLink */
+    a {
+        padding: 2px 4px;
+        padding-bottom: 4px;
+        display: block;
+        border: 1px solid transparent;
+        border-radius: 4px 4px 0 0;
 
-    button {
-      padding: 4px 16px;
-      max-height: unset;
-    }
-
-    &.active {
-      background-color: var(--md-sys-color-background);
-      border: 1px solid var(--md-sys-color-outline-variant);
- 
-
-      button {
-        &:hover {
-          background-color: unset;
+        /* this will be fixed with theme buttons */
+        button {
+            padding: 4px 16px;
+            max-height: unset;
         }
-      }
 
-      border-bottom: 1px solid var(--md-sys-color-background);
+        &.active {
+            background-color: var(--md-sys-color-background);
+            border: 1px solid var(--md-sys-color-outline-variant);
+            border-bottom: 1px solid var(--color-grey-01);
+
+            button {
+                &:hover {
+                    background-color: unset;
+                }
+            }
+
+            border-bottom: 1px solid var(--md-sys-color-background);
+        }
     }
-  }
 `
 
 export const Views = styled.span`
-  display: flex;
-  align-items: center;
-  margin-left: -8px;
+    display: flex;
+    align-items: center;
+    margin-left: -8px;
 `
