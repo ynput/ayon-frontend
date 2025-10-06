3432486aeaa586564acbe23acbfddcbc73d37e1f
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-10-06 04:50:40 +0200

    feat(details): Cleanup

--- src/hooks/useTableLoadingData.ts
+++ src/hooks/useTableLoadingData.ts
@@ -1,30 +1,21 @@
 import { useMemo } from 'react'
 
-interface LoadingTableItem {
-  [key: string]: string | number | boolean | Record<string, unknown>
-  data: Record<string, unknown>
-  isLoading: boolean
-}
+const useTableLoadingData = (data: any, isLoading: boolean, number = 10, dataKey?: string) => {
+  let tableData = data
 
-const useTableLoadingData = <T = unknown>(
-  data: T[] | undefined,
-  isLoading: boolean,
-  number = 10,
-  dataKey?: string
-): (T | LoadingTableItem)[] => {
   const loadingData = useMemo(() => {
     return Array.from({ length: number }, (_, i) => ({
       [dataKey || 'key']: i.toString(),
       data: {},
       isLoading: true,
     }))
-  }, [number, dataKey])
+  }, [])
 
   if (isLoading) {
-    return loadingData
+    tableData = loadingData
   }
 
-  return data || []
+  return tableData
 }
 
 export default useTableLoadingData
