8f616803ade14298d041820f42a5882a307f92c5
Innders <49156310+Innders@users.noreply.github.com>
2026-01-21 16:06:36 +0000

    fix: provide extra props to subtasks manager module

--- shared/src/components/SubtasksManager/SubtasksManagerWrapper.tsx
+++ shared/src/components/SubtasksManager/SubtasksManagerWrapper.tsx
@@ -6,6 +6,7 @@ import {
   useUpdateOverviewEntitiesMutation,
 } from '@shared/api'
 import { useSubtasksModulesContext } from '@shared/context'
+import { useNavigate } from 'react-router-dom'
 
 export type UpdateOverviewEntities = ReturnType<typeof useUpdateOverviewEntitiesMutation>[0]
 
@@ -18,15 +19,22 @@ export interface SubtasksManagerProps extends React.HTMLAttributes<HTMLDivElemen
   onClose?: () => void
   onSelectSubtasks?: (subtaskIds: string[]) => void
   updateOverviewEntities: UpdateOverviewEntities
+  useNavigate: typeof useNavigate
 }
 
 export const SubtasksManagerWrapper = (
-  props: Omit<SubtasksManagerProps, 'updateOverviewEntities' | 'users'>,
+  props: Omit<SubtasksManagerProps, 'updateOverviewEntities' | 'users' | 'useNavigate'>,
 ) => {
   const { SubtasksManager } = useSubtasksModulesContext()
   const [updateOverviewEntities] = useUpdateOverviewEntitiesMutation()
   const { data: users = [] } = useGetUsersQuery({})
+
   return (
-    <SubtasksManager {...props} updateOverviewEntities={updateOverviewEntities} users={users} />
+    <SubtasksManager
+      {...props}
+      updateOverviewEntities={updateOverviewEntities}
+      users={users}
+      useNavigate={useNavigate}
+    />
   )
 }
