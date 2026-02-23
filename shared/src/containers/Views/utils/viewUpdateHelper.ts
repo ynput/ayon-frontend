073b3b0d2bf0b7c29d087da586546160135780b5
filipvnencak <vnencak.filip@gmail.com>
2026-02-23 07:46:11 +0100

    fix(views): skip view settings update when undefined

--- shared/src/containers/Views/utils/viewUpdateHelper.ts
+++ shared/src/containers/Views/utils/viewUpdateHelper.ts
@@ -55,6 +55,11 @@ export const updateViewSettings = async (
   const wasWorking = selectedView?.working
 
   try {
+    if (viewSettings === undefined) {
+      console.warn('updateViewSettings called while viewSettings is undefined; skipping update')
+      return
+    }
+
     // Immediately update local state for fast UI response
     localStateSetter(newLocalValue)
 
