fba962834f85ec42fd5e4cbe5283178cff01ed07
jakubfiala <j4kub.fiala@gmail.com>
2026-03-24 10:33:29 +0100

    add row numbers to data preview

--- src/components/ImportDialog/components/DataPreview.tsx
+++ src/components/ImportDialog/components/DataPreview.tsx
@@ -1,6 +1,7 @@
 import { Panel } from "@ynput/ayon-react-components"
 import { ImportData } from "../utils"
 import styled from "styled-components"
+import { CSSProperties, useMemo } from "react"
 
 type Props = {
   data: ImportData
@@ -8,13 +9,32 @@ type Props = {
 }
 
 const List = styled.ol`
+  --max-row-index-length: 2;
+
   background: var(--md-sys-color-surface-container-low);
   list-style-type: none;
   padding: 0;
   margin: 0;
+  counter-reset: import-data-preview-row;
 `
 const Item = styled.li`
   padding: var(--padding-m);
+  counter-increment: import-data-preview-row;
+  display: flex;
+  gap: var(--base-gap-medium);
+
+  span {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    flex-shrink: 1;
+  }
+
+  &::before {
+    content: counters(import-data-preview-row, ".", decimal);
+    color: var(--md-sys-color-outline);
+    width: calc(var(--max-row-index-length) * 1ch);
+    display: inline-block;
+  }
 
   & + & {
     border-top: 1px solid var(--md-sys-color-surface-container);
@@ -44,8 +64,13 @@ function Placeholder() {
 export default function DataPreview({ data, column }: Props) {
   if (!column) return <Placeholder />
 
+  const maxRowIndexLength = useMemo(
+    () => data.rows.length.toString().length,
+    [data.rows]
+  )
+
   return (
-    <List>
+    <List style={{ '--max-row-index-length': maxRowIndexLength } as unknown as CSSProperties}>
       {
         data.rows.map((row, index) => (
           <Item key={index}>
