4f908a03ca6ac46732592c3017575af61b4bece4
Innders <49156310+Innders@users.noreply.github.com>
2026-04-14 15:18:44 +0100

    fix: EnumEditor fixes including false onChange events

--- shared/src/components/EnumEditor/EnumEditorItem.tsx
+++ shared/src/components/EnumEditor/EnumEditorItem.tsx
@@ -5,8 +5,31 @@ import { DropdownRef, Icon } from '@ynput/ayon-react-components'
 
 import * as Styled from './EnumEditor.styled'
 import { AttributeData } from './EnumEditor'
+import clsx from 'clsx'
 
-interface EnumEditorItemProps {
+type DivPt = Partial<React.HTMLAttributes<HTMLDivElement>>
+
+type TextInputPt = Partial<React.ComponentProps<typeof Styled.InputText>>
+type IconInputPt = Partial<React.ComponentProps<typeof Styled.IconSelect>>
+type ColorInputPt = Partial<React.InputHTMLAttributes<HTMLInputElement>>
+
+export interface EnumEditorFieldPt<TInput> {
+  wrapper?: DivPt
+  label?: DivPt
+  input?: Partial<TInput>
+}
+
+export interface EnumEditorItemPt {
+  wrapper?: DivPt
+  label?: EnumEditorFieldPt<TextInputPt>
+  value?: EnumEditorFieldPt<TextInputPt>
+  icon?: EnumEditorFieldPt<IconInputPt>
+  color?: EnumEditorFieldPt<ColorInputPt>
+  footer?: DivPt
+}
+
+export interface EnumEditorItemProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
   item: AttributeData
   onChange?: (attr: (keyof AttributeData)[], value: (boolean | string | undefined)[]) => void
   onRemove?: () => void
@@ -15,6 +38,7 @@ interface EnumEditorItemProps {
   showDuplicateButton?: boolean
   autoFocus?: boolean
   isExpanded?: boolean
+  pt?: EnumEditorItemPt
 }
 
 const EnumEditorItem = ({
@@ -26,6 +50,9 @@ const EnumEditorItem = ({
   showDuplicateButton = true,
   autoFocus = false,
   isExpanded = true,
+  className,
+  pt,
+  ...props
 }: EnumEditorItemProps) => {
   const { label, value, icon, color, isNewAttribute } = item
   const labelRef = useRef<HTMLInputElement>(null)
@@ -48,13 +75,22 @@ const EnumEditorItem = ({
   }, [isExpanded, autoFocus, item.isNewAttribute])
 
   return (
-    <Styled.EnumItemBody className={isExpanded ? 'expanded' : 'collapsed'}>
-      <Styled.Row key="label">
-        <Styled.Label>Label</Styled.Label>
+    <Styled.EnumItemBody
+      {...pt?.wrapper}
+      {...props}
+      className={clsx(className, pt?.wrapper?.className, {
+        expanded: isExpanded,
+        collapsed: !isExpanded,
+      })}
+    >
+      <Styled.Row key="label" {...pt?.label?.wrapper}>
+        <Styled.Label {...pt?.label?.label}>Label</Styled.Label>
         <Styled.InputText
           ref={labelRef}
           value={label}
           autoFocus={autoFocus || isNewAttribute}
+          {...pt?.label?.input}
+          className={clsx(pt?.label?.input?.className)}
           onChange={(event) => {
             if (!isNewAttribute) {
               return onChange && onChange(['label'], [event.target.value])
@@ -69,18 +105,20 @@ const EnumEditorItem = ({
         />
       </Styled.Row>
 
-      <Styled.Row key="value">
-        <Styled.Label>Value</Styled.Label>
+      <Styled.Row key="value" {...pt?.value?.wrapper}>
+        <Styled.Label {...pt?.value?.label}>Value</Styled.Label>
         <Styled.InputText
           ref={valueRef}
           value={valueRef.current?.value || value}
+          {...pt?.value?.input}
+          className={clsx(pt?.value?.input?.className)}
           onChange={(event) => onChange && onChange(['value'], [event.target.value])}
           placeholder="Enter value"
         />
       </Styled.Row>
 
-      <Styled.Row key="icon">
-        <Styled.Label>Icon</Styled.Label>
+      <Styled.Row key="icon" {...pt?.icon?.wrapper}>
+        <Styled.Label {...pt?.icon?.label}>Icon</Styled.Label>
 
         <Styled.PlaceholderWrapper style={{ position: 'relative' }}>
           <Styled.Placeholder
@@ -96,9 +134,12 @@ const EnumEditorItem = ({
             ref={iconSelectRef}
             value={[icon || 'question_mark']}
             widthExpand
+            {...pt?.icon?.input}
+            className={clsx(pt?.icon?.input?.className)}
             style={{
               position: icon ? 'relative' : 'absolute',
               visibility: icon ? 'visible' : 'hidden',
+              ...pt?.icon?.input?.style,
             }}
             onChange={(value) => {
               return onChange && onChange(['icon'], [value[0]])
@@ -116,8 +157,8 @@ const EnumEditorItem = ({
         </Styled.PlaceholderWrapper>
       </Styled.Row>
 
-      <Styled.Row key="color">
-        <Styled.Label>Color</Styled.Label>
+      <Styled.Row key="color" {...pt?.color?.wrapper}>
+        <Styled.Label {...pt?.color?.label}>Color</Styled.Label>
 
         <Styled.PlaceholderWrapper style={{ position: 'relative' }}>
           <Styled.ColorPicker
@@ -130,6 +171,8 @@ const EnumEditorItem = ({
               type="color"
               ref={colorPickerRef}
               value={color || '#000000'}
+              {...pt?.color?.input}
+              className={clsx(pt?.color?.input?.className)}
               onChange={(event) =>
                 onChange && onChange(['color'], [event?.target.value.toString()])
               }
@@ -148,7 +191,7 @@ const EnumEditorItem = ({
       </Styled.Row>
 
       {(showRemoveButton || showDuplicateButton) && (
-        <Styled.Row className="footer">
+        <Styled.Row {...pt?.footer} className={clsx('footer', pt?.footer?.className)}>
           {showRemoveButton && (
             <Styled.ActionWrapper>
               <Styled.Button icon="close" variant="text" onClick={onRemove}>
