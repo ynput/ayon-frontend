304c2dc8733bf6fca55342aa12a60738481aa952
filipvnencak <vnencak.filip@gmail.com>
2026-04-27 14:17:46 +0200

    fix(graphql-codegen): inject TypedDocumentString runtime class

--- shared/src/api/generated/graphqlLinks.ts
+++ shared/src/api/generated/graphqlLinks.ts
@@ -18,6 +18,18 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+class TypedDocumentString<TResult, TVariables> extends String {
+  __apiType?: (variables: TVariables) => TResult
+  __meta__?: Record<string, any>
+  constructor(value: string, __meta__?: Record<string, any>) {
+    super(value)
+    this.__meta__ = __meta__
+  }
+  toString(): string {
+    return this.valueOf()
+  }
+}
+
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
